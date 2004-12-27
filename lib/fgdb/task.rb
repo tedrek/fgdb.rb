c56b2cb37b6371133208215e14ef5f6806d12a6d
Martin Chase <stillflame@dev.freegeek.org>
2004-12-27 09:05:04 +0000

    - added preliminary tasks

---
+++ lib/fgdb/task.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+#
+#
+#
+# == Subversion ID
+# 
+# $Id$
+# 
+# == Authors
+# 
+# * Martin Chase <mchase@freegeek.org>
+# 
+
+class FGDB::Task
+
+	# SVN Revision
+	SVNRev = %q$Rev$
+
+	# SVN Id
+	SVNId = %q$Id$
+
+	# SVN URL
+	SVNURL = %q$URL$
+
+end # class FGDB::Task
