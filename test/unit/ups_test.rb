818d58e8fb32497feaf58c85a04eb4d79efb7d15
Martin Chase <stillflame@dev.freegeek.org>
2006-06-11 02:48:51 +0000

    - renaming upse to ups

--- test/unit/ups_test.rb
+++ test/unit/ups_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class UpseTest < Test::Unit::TestCase
-  fixtures :gizmos, :upses
+class UpsTest < Test::Unit::TestCase
+  fixtures :gizmos, :ups
 
   # Replace this with your real tests.
   def test_truth
