3b4ebd4db8af39d7128cab3fa8c31af968feec78
Martin Chase <stillflame@dev.freegeek.org>
2006-09-19 21:23:13 +0000

    - tab replacement

--- test/unit/contact_type_test.rb
+++ test/unit/contact_type_test.rb
@@ -3,9 +3,9 @@ require File.dirname(__FILE__) + '/../test_helper'
 class ContactTypeTest < Test::Unit::TestCase
   fixtures :contact_types
 
-	NEW_CONTACT_TYPE = {}	# e.g. {:name => 'Test ContactType', :description => 'Dummy'}
-	REQ_ATTR_NAMES 			 = %w( ) # name of fields that must be present, e.g. %(name description)
-	DUPLICATE_ATTR_NAMES = %w( ) # name of fields that cannot be a duplicate, e.g. %(name description)
+  NEW_CONTACT_TYPE = {}    # e.g. {:name => 'Test ContactType', :description => 'Dummy'}
+  REQ_ATTR_NAMES              = %w( ) # name of fields that must be present, e.g. %(name description)
+  DUPLICATE_ATTR_NAMES = %w( ) # name of fields that cannot be a duplicate, e.g. %(name description)
 
   def setup
     # Retrieve fixtures via their name
@@ -23,31 +23,31 @@ class ContactTypeTest < Test::Unit::TestCase
     end
   end
 
-	def test_new
+  def test_new
     contact_type = ContactType.new(NEW_CONTACT_TYPE)
     assert contact_type.valid?, "ContactType should be valid"
-   	NEW_CONTACT_TYPE.each do |attr_name|
+    NEW_CONTACT_TYPE.each do |attr_name|
       assert_equal NEW_CONTACT_TYPE[attr_name], contact_type.attributes[attr_name], "ContactType.@#{attr_name.to_s} incorrect"
     end
- 	end
+  end
 
-	def test_validates_presence_of
-   	REQ_ATTR_NAMES.each do |attr_name|
-			tmp_contact_type = NEW_CONTACT_TYPE.clone
-			tmp_contact_type.delete attr_name.to_sym
-			contact_type = ContactType.new(tmp_contact_type)
-			assert !contact_type.valid?, "ContactType should be invalid, as @#{attr_name} is invalid"
-    	assert contact_type.errors.invalid?(attr_name.to_sym), "Should be an error message for :#{attr_name}"
+  def test_validates_presence_of
+    REQ_ATTR_NAMES.each do |attr_name|
+      tmp_contact_type = NEW_CONTACT_TYPE.clone
+      tmp_contact_type.delete attr_name.to_sym
+      contact_type = ContactType.new(tmp_contact_type)
+      assert !contact_type.valid?, "ContactType should be invalid, as @#{attr_name} is invalid"
+      assert contact_type.errors.invalid?(attr_name.to_sym), "Should be an error message for :#{attr_name}"
     end
- 	end
+  end
 
-	def test_duplicate
+  def test_duplicate
     current_contact_type = ContactType.find_first
-   	DUPLICATE_ATTR_NAMES.each do |attr_name|
-   		contact_type = ContactType.new(NEW_CONTACT_TYPE.merge(attr_name.to_sym => current_contact_type[attr_name]))
-			assert !contact_type.valid?, "ContactType should be invalid, as @#{attr_name} is a duplicate"
-    	assert contact_type.errors.invalid?(attr_name.to_sym), "Should be an error message for :#{attr_name}"
-		end
-	end
+    DUPLICATE_ATTR_NAMES.each do |attr_name|
+      contact_type = ContactType.new(NEW_CONTACT_TYPE.merge(attr_name.to_sym => current_contact_type[attr_name]))
+      assert !contact_type.valid?, "ContactType should be invalid, as @#{attr_name} is a duplicate"
+      assert contact_type.errors.invalid?(attr_name.to_sym), "Should be an error message for :#{attr_name}"
+    end
+  end
 end
 
