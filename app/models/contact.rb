9aec84901850e30d441e9274031b20f8c2e92f5c
gp <gp@dev.freegeek.org>
2006-09-20 18:33:30 +0000

    Recreate model relationships in contact.rb after ajax scaffold   generate wiped them out.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,59 @@
 require 'ajax_scaffold'
 
 class Contact < ActiveRecord::Base
+  
+  has_and_belongs_to_many :contact_types
+  has_many :contact_methods, :dependent => true
+  has_many :contact_method_types, :through => :contact_methods
+
+  # many to many self referential relationship (source and sink can be
+  # used to determine direction of relationship)
+  has_many :relationships_as_source, :foreign_key => 'source_id', :class_name => 'Relationship'
+  has_many :relationships_as_sink,   :foreign_key => 'sink_id',   :class_name => 'Relationship'
+  has_many :sources,  :through => :relationships_as_sink
+  has_many :sinks,    :through => :relationships_as_source
+
+  acts_as_userstamp
+
+  def is_organization?
+    self.is_organization
+  end
+
+  def is_person?
+    ! self.is_organization?
+  end
+
+  def display_name
+    if is_person?
+      if (self.first_name || self.middle_name || self.surname) &&
+          !(self.first_name + self.middle_name + self.surname).empty?
+        "%s %s %s" % [self.first_name, self.middle_name, self.surname]
+      else
+        '(person without name)'
+      end
+    else
+      if self.organization && ! self.organization.empty?
+        self.organization
+      else
+        '(organization without name)'
+      end
+    end
+  end
+
+  def relationships
+    (relationships_as_source + relationships_as_sink).uniq
+  end
+
+  class << self
+
+    def people
+      find(:all, :conditions => ["is_organization = ?", false])
+    end
+
+    def organizations
+      find(:all, :conditions => ["is_organization = ?", true])
+    end
+
+  end
 
 end
