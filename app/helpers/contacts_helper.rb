d8a1b9041063d06f64dea33725ce2360f4af2e55
gp <gp@dev.freegeek.org>
2006-09-22 17:29:04 +0000

    set initial scaffold_columns for contacts

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -6,7 +6,34 @@ module ContactsHelper
   end
   
   def scaffold_columns
-    Contact.scaffold_columns
+    [ 
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'is_organization'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'sort_name'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'first_name'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'middle_name'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'surname'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'organization'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'extra_address'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'address'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'city'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'state_or_province'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'postal_code'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'country'),
+      AjaxScaffold::ScaffoldColumn.new(Contact, 
+        :name => 'notes'),
+    ]
   end
 
 end
