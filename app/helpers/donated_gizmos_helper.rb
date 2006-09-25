44ca41e32b787acd8a693df46ee3c2651165fdd0
gp <gp@dev.freegeek.org>
2006-09-25 19:39:44 +0000

    Fix some glaring errors to DonatedGizmo display; bugs persist.

--- app/helpers/donated_gizmos_helper.rb
+++ app/helpers/donated_gizmos_helper.rb
@@ -6,13 +6,15 @@ module DonatedGizmosHelper
   end
   
   def scaffold_columns
-    DonatedGizmo.scaffold_columns
     [ 
       AjaxScaffold::ScaffoldColumn.new(DonatedGizmo, 
         :name => 'gizmo_type_id', :label => 'Donated gizmo type', 
         :eval => 'donated_gizmo.gizmo_type', :sortable => false),
       AjaxScaffold::ScaffoldColumn.new(DonatedGizmo, 
-        :name => 'description'),
+        :name => 'quantity'),
+      AjaxScaffold::ScaffoldColumn.new(DonatedGizmo, 
+        :name => 'donation_id', :label => 'Donation Id', 
+        :eval => 'donated_gizmo.donation', :sortable => false),
     ]
   end
 
