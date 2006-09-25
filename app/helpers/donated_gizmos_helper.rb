09fb484fae7613113e8902af54a8cb67f5e70e12
gp <gp@dev.freegeek.org>
2006-09-25 18:58:52 +0000

    Clean up display for DonatedGizmo, GizmoType by starting to use   custom scaffold list in helper file, non-default controls in   _form.rhtml (ie, the usual stuff). Remove "hidden" fields from Donation _form.rhtml.

--- app/helpers/donated_gizmos_helper.rb
+++ app/helpers/donated_gizmos_helper.rb
@@ -7,6 +7,13 @@ module DonatedGizmosHelper
   
   def scaffold_columns
     DonatedGizmo.scaffold_columns
+    [ 
+      AjaxScaffold::ScaffoldColumn.new(DonatedGizmo, 
+        :name => 'gizmo_type_id', :label => 'Donated gizmo type', 
+        :eval => 'donated_gizmo.gizmo_type', :sortable => false),
+      AjaxScaffold::ScaffoldColumn.new(DonatedGizmo, 
+        :name => 'description'),
+    ]
   end
 
 end
--- app/helpers/gizmo_types_helper.rb
+++ app/helpers/gizmo_types_helper.rb
@@ -6,7 +6,38 @@ module GizmoTypesHelper
   end
   
   def scaffold_columns
-    GizmoType.scaffold_columns
+     [ 
+      AjaxScaffold::ScaffoldColumn.new(GizmoType, 
+        :name => 'parent_id', :label => 'Parent Type', 
+        :eval => 'gizmo_type.parent.description', :sortable => false),
+      AjaxScaffold::ScaffoldColumn.new(GizmoType, 
+        :name => 'description'),
+      AjaxScaffold::ScaffoldColumn.new(GizmoType, 
+        :name => 'fee'),
+      AjaxScaffold::ScaffoldColumn.new(GizmoType, 
+        :name => 'fee_is_required', :label => "Fee Required?",
+        :eval => 'gizmo_type.fee_is_required ? "yes" : "no"'),
+      AjaxScaffold::ScaffoldColumn.new(GizmoType, 
+        :name => 'instantiable', :label => "Instantiable?",
+        :eval => 'gizmo_type.instantiable ? "yes" : "no"'),
+      ]
+  end
+
+end
+module RelationshipTypesHelper
+  include AjaxScaffold::Helper
+  
+  def num_columns
+    scaffold_columns.length + 1 
+  end
+  
+  def scaffold_columns
+     [ AjaxScaffold::ScaffoldColumn.new(RelationshipType, 
+        :name => 'description'),
+      AjaxScaffold::ScaffoldColumn.new(RelationshipType, 
+        :name => 'direction_matters', :label => "Does direction matter?",
+        :eval => 'relationship_type.direction_matters ? "yes" : "no"'),
+      ]
   end
 
 end
