79d70a255f91a8c368c6b2e1774df7dc4e2b3708
Martin Chase <stillflame@dev.freegeek.org>
2006-11-14 21:57:43 +0000

    - adding stubs for layouts and the first two reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,23 @@
 class ReportsController < ApplicationController
+
+  layout :report_layout_choice
+  def report_layout_choice
+    case action_name
+    when /report$/ then 'reports_display.rhtml'
+    else                'reports_form.rhtml'
+    end
+  end
+
+  def volunteer_tasks
+  end
+
+  def volunteer_tasks_report
+  end
+
+  def income
+  end
+
+  def income_report
+  end
+
 end
