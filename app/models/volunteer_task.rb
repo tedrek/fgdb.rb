bef6a33c5dec3c198ba1056230be001b97f28c04
Martin Chase <stillflame@dev.freegeek.org>
2006-12-15 20:24:08 +0000

    - make sure the date is there

--- app/models/volunteer_task.rb
+++ app/models/volunteer_task.rb
@@ -4,6 +4,8 @@ class VolunteerTask < ActiveRecord::Base
   has_and_belongs_to_many :volunteer_task_types
   belongs_to  :contact, :order => "surname, first_name"
 
+  validates_presence_of :date_performed
+
   def effective_duration
     mult = volunteer_task_types.inject(1) do |mu,type|
       mu *= type.hours_multiplier
