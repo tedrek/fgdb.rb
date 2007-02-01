051e411bf5f3143711970955fc0844c36f7e13c7
Martin Chase <stillflame@dev.freegeek.org>
2007-02-01 01:56:16 +0000

    removed unused code

--- app/models/source_type.rb
+++
@@ -1,9 +0,0 @@
-require 'ajax_scaffold'
-
-class SourceType < ActiveRecord::Base
-  validates_uniqueness_of :description, :message => "already exists"
-
-  def to_s
-    description
-  end
-end
--- app/models/till_handler.rb
+++
@@ -1,9 +0,0 @@
-require 'ajax_scaffold'
-
-class TillHandler < ActiveRecord::Base
-  belongs_to :contact, :order => "surname, first_name"
-  def to_s
-    description
-  end
-
-end
