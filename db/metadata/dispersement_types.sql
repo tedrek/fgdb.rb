3b8da92315ae7bc6511c53397862e12f68bc64e9
Martin Chase <stillflame@dev.freegeek.org>
2007-04-05 00:36:08 +0000

    - dispersement types i know we need

--- db/metadata/dispersement_types.sql
+++ db/metadata/dispersement_types.sql
@@ -12,7 +12,7 @@ SET search_path = public, pg_catalog;
 -- Name: dispersement_types_id_seq; Type: SEQUENCE SET; Schema: public; Owner: stillflame
 --
 
-SELECT pg_catalog.setval(pg_catalog.pg_get_serial_sequence('dispersement_types', 'id'), 1, false);
+SELECT pg_catalog.setval(pg_catalog.pg_get_serial_sequence('dispersement_types', 'id'), 5, true);
 
 
 --
@@ -22,6 +22,11 @@ SELECT pg_catalog.setval(pg_catalog.pg_get_serial_sequence('dispersement_types',
 ALTER TABLE dispersement_types DISABLE TRIGGER ALL;
 
 COPY dispersement_types (id, description, lock_version, updated_at, created_at, created_by, updated_by) FROM stdin;
+1	Adoption	0	2007-04-04 17:26:09	2007-04-04 17:25:00	1	1
+2	Build	0	2007-04-04 17:26:16	2007-04-04 17:26:00	1	1
+3	Hardware Grants	0	2007-04-04 17:26:28	2007-04-04 17:26:00	1	1
+4	GAP	0	2007-04-04 17:26:37	2007-04-04 17:26:00	1	1
+5	Staff	0	2007-04-04 17:26:45	2007-04-04 17:26:00	1	1
 \.
 
 
