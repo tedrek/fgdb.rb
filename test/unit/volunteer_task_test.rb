a819a77dd9c9435281361a383cad972aef333f1d
Martin Chase <stillflame@dev.freegeek.org>
2007-08-01 20:06:14 +0000

    - fixed volunteer_task and contact_method tests to not break from mysterious data troubles

--- test/unit/volunteer_task_test.rb
+++ test/unit/volunteer_task_test.rb
@@ -1,14 +1,18 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class VolunteerTaskTest < Test::Unit::TestCase
-  fixtures :volunteer_tasks, :volunteer_task_types
+  fixtures :volunteer_task_types, :volunteer_tasks
 
-  NEW_VOLUNTEER_TASK = {
-    :duration => 1.5,
-    :date_performed => Date.today,
-    :contact_id => 1,
-    :volunteer_task_types => [VolunteerTaskType.find(46)]
-  }
+  def new_volunteer_task
+    type = VolunteerTaskType.new({:instantiable => true, :description => 'meowing', :hours_multiplier => 1.0, :required => true})
+    type.save
+    {
+      :duration => 1.5,
+      :date_performed => Date.today,
+      :contact_id => 1,
+      :volunteer_task_types => [type]
+    }
+  end
   REQ_ATTR_NAMES 			 = %w( contact_id date_performed) # name of fields that must be present, e.g. %(name description)
   DUPLICATE_ATTR_NAMES = %w( ) # name of fields that cannot be a duplicate, e.g. %(name description)
 
@@ -29,16 +33,16 @@ class VolunteerTaskTest < Test::Unit::TestCase
   end
 
   def test_new
-    volunteer_task = VolunteerTask.new(NEW_VOLUNTEER_TASK)
+    volunteer_task = VolunteerTask.new(new_volunteer_task)
     assert volunteer_task.valid?, "VolunteerTask should be valid"
-   	NEW_VOLUNTEER_TASK.each do |attr_name|
-      assert_equal NEW_VOLUNTEER_TASK[attr_name], volunteer_task.attributes[attr_name], "VolunteerTask.@#{attr_name.to_s} incorrect"
+   	new_volunteer_task.each do |attr_name|
+      assert_equal new_volunteer_task[attr_name], volunteer_task.attributes[attr_name], "VolunteerTask.@#{attr_name.to_s} incorrect"
     end
   end
 
   def test_validates_presence_of
    	REQ_ATTR_NAMES.each do |attr_name|
-			tmp_volunteer_task = NEW_VOLUNTEER_TASK.clone
+			tmp_volunteer_task = new_volunteer_task.clone
 			tmp_volunteer_task.delete attr_name.to_sym
 			volunteer_task = VolunteerTask.new(tmp_volunteer_task)
 			assert !volunteer_task.valid?, "VolunteerTask should be invalid, as @#{attr_name} is invalid"
@@ -49,7 +53,7 @@ class VolunteerTaskTest < Test::Unit::TestCase
   def test_duplicate
     current_volunteer_task = VolunteerTask.find_first
    	DUPLICATE_ATTR_NAMES.each do |attr_name|
-      volunteer_task = VolunteerTask.new(NEW_VOLUNTEER_TASK.merge(attr_name.to_sym => current_volunteer_task[attr_name]))
+      volunteer_task = VolunteerTask.new(new_volunteer_task.merge(attr_name.to_sym => current_volunteer_task[attr_name]))
       assert !volunteer_task.valid?, "VolunteerTask should be invalid, as @#{attr_name} is a duplicate"
       assert volunteer_task.errors.invalid?(attr_name.to_sym), "Should be an error message for :#{attr_name}"
     end
