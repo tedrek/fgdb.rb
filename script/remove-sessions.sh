e75866fb6441b5667aa3b0d3fc88ed7a43bb4095
Martin Chase <stillflame@dev.freegeek.org>
2007-08-01 20:23:28 +0000

    - applies patch from edavis10 to delete old session files

---
+++ script/remove-sessions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "--help" = "$1" ]
+then
+    echo "Remove old session files older than HOURS (default 24)"
+    echo
+    echo "Usage: RAILS_ROOT='/path' remove_sessions.sh [HOURS]"
+    echo "       script/remove_sessions.sh [HOURS] (will use current directory)"
+    echo
+    exit
+fi
+
+
+# Default to 24 hours if not set
+if [ -z $1 ]
+then
+    TIME=$(expr 24 \* 60)
+else
+    TIME=$(expr $1 \* 60)
+fi
+
+# Use the current directory for the RAILS_ROOT if it was not set
+if [ -z $RAILS_ROOT ]
+then
+    RAILS_ROOT=`pwd`
+fi
+
+find $RAILS_ROOT/tmp/sessions -mmin +$TIME -iname 'ruby_sess.*' -exec rm -v {} \;
