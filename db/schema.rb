80ba4ee72f1891a6b1170ccf4d72ccc4a18e9ad2
Martin Chase <stillflame@dev.freegeek.org>
2007-08-04 17:43:52 +0000

    - added the database migration from the previous patch (with slight modifications)

--- db/schema.rb
+++ db/schema.rb
@@ -88,7 +88,7 @@ ActiveRecord::Schema.define(:version => 7) do
   create_table "discount_schedules_gizmo_types", :force => true do |t|
     t.column "gizmo_type_id", :integer, :null => false
     t.column "discount_schedule_id", :integer, :null => false
-    t.column "multiplier", :float
+    t.column "multiplier", :float, :limit => 10
     t.column "lock_version", :integer, :default => 0, :null => false
     t.column "updated_at", :datetime
     t.column "created_at", :datetime
@@ -122,8 +122,8 @@ ActiveRecord::Schema.define(:version => 7) do
   create_table "donations", :force => true do |t|
     t.column "contact_id", :integer
     t.column "postal_code", :string, :limit => 25
-    t.column "reported_required_fee", :float, :default => 0.0
-    t.column "reported_suggested_fee", :float, :default => 0.0
+    t.column "reported_required_fee", :float, :limit => 10, :default => 0.0
+    t.column "reported_suggested_fee", :float, :limit => 10, :default => 0.0
     t.column "txn_complete", :boolean, :default => true
     t.column "txn_completed_at", :datetime
     t.column "comments", :text
@@ -202,7 +202,7 @@ ActiveRecord::Schema.define(:version => 7) do
     t.column "attr_val_text", :text
     t.column "attr_val_boolean", :boolean
     t.column "attr_val_integer", :integer
-    t.column "attr_val_monetary", :float
+    t.column "attr_val_monetary", :float, :limit => 10
     t.column "lock_version", :integer, :default => 0, :null => false
     t.column "updated_at", :datetime
     t.column "created_at", :datetime
@@ -230,13 +230,13 @@ ActiveRecord::Schema.define(:version => 7) do
     t.column "created_at", :datetime
     t.column "created_by", :integer, :default => 1, :null => false
     t.column "updated_by", :integer, :default => 1, :null => false
-    t.column "required_fee", :float, :default => 0.0
-    t.column "suggested_fee", :float, :default => 0.0
+    t.column "required_fee", :float, :limit => 10, :default => 0.0
+    t.column "suggested_fee", :float, :limit => 10, :default => 0.0
   end
 
   create_table "high_sales", :id => false, :force => true do |t|
     t.column "contact_id", :integer
-    t.column "reported_discount_amount", :float
+    t.column "reported_discount_amount", :float, :limit => 10
     t.column "open_window", :date
     t.column "close_window", :date
     t.column "first_name", :string, :limit => 25
@@ -261,7 +261,7 @@ ActiveRecord::Schema.define(:version => 7) do
   create_table "payments", :force => true do |t|
     t.column "donation_id", :integer
     t.column "sale_id", :integer
-    t.column "amount", :float, :default => 0.0, :null => false
+    t.column "amount", :float, :limit => 10, :default => 0.0, :null => false
     t.column "payment_method_id", :integer, :null => false
     t.column "lock_version", :integer, :default => 0, :null => false
     t.column "updated_at", :datetime
@@ -288,8 +288,8 @@ ActiveRecord::Schema.define(:version => 7) do
   create_table "sales", :force => true do |t|
     t.column "contact_id", :integer
     t.column "postal_code", :string, :limit => 25
-    t.column "reported_discount_amount", :float, :default => 0.0
-    t.column "reported_amount_due", :float, :default => 0.0, :null => false
+    t.column "reported_discount_amount", :float, :limit => 10, :default => 0.0
+    t.column "reported_amount_due", :float, :limit => 10, :default => 0.0, :null => false
     t.column "txn_complete", :boolean, :default => true
     t.column "txn_completed_at", :datetime
     t.column "discount_schedule_id", :integer
@@ -306,7 +306,7 @@ ActiveRecord::Schema.define(:version => 7) do
 
   create_table "sales_violations", :id => false, :force => true do |t|
     t.column "contact_id", :integer
-    t.column "reported_discount_amount", :float
+    t.column "reported_discount_amount", :float, :limit => 10
     t.column "open_window", :date
     t.column "close_window", :date
     t.column "first_name", :string, :limit => 25
@@ -332,7 +332,7 @@ ActiveRecord::Schema.define(:version => 7) do
   create_table "volunteer_task_types", :force => true do |t|
     t.column "description", :string, :limit => 100
     t.column "parent_id", :integer
-    t.column "hours_multiplier", :float, :default => 1.0, :null => false
+    t.column "hours_multiplier", :float, :limit => 10, :default => 1.0, :null => false
     t.column "instantiable", :boolean, :default => true, :null => false
     t.column "lock_version", :integer, :default => 0, :null => false
     t.column "updated_at", :datetime
@@ -352,7 +352,7 @@ ActiveRecord::Schema.define(:version => 7) do
   create_table "volunteer_tasks", :force => true do |t|
     t.column "contact_id", :integer
     t.column "date_performed", :date
-    t.column "duration", :float, :default => 0.0, :null => false
+    t.column "duration", :float, :limit => 5, :default => 0.0, :null => false
     t.column "lock_version", :integer, :default => 0, :null => false
     t.column "updated_at", :datetime
     t.column "created_at", :datetime
@@ -376,7 +376,7 @@ ActiveRecord::Schema.define(:version => 7) do
     v.column :total_paid
   end
 
-  create_view "v_donations", "SELECT d.id, d.contact_id, d.postal_code, d.reported_required_fee, d.reported_suggested_fee, d.txn_complete, d.txn_completed_at, d.comments, d.lock_version, d.updated_at, d.created_at, d.created_by, d.updated_by, v.total_paid, CASE WHEN (v.total_paid > d.reported_required_fee) THEN d.reported_required_fee ELSE v.total_paid END AS fees_paid, CASE WHEN (v.total_paid < d.reported_required_fee) THEN (0.00)::real ELSE (v.total_paid - d.reported_required_fee) END AS donations_paid FROM (donations d JOIN v_donation_totals v ON ((d.id = v.id)));", :force => true do |v|
+  create_view "v_donations", "SELECT d.id, d.contact_id, d.postal_code, d.reported_required_fee, d.reported_suggested_fee, d.txn_complete, d.txn_completed_at, d.comments, d.lock_version, d.updated_at, d.created_at, d.created_by, d.updated_by, v.total_paid, CASE WHEN (v.total_paid > d.reported_required_fee) THEN d.reported_required_fee ELSE v.total_paid END AS fees_paid, CASE WHEN (v.total_paid < d.reported_required_fee) THEN 0.00 ELSE (v.total_paid - d.reported_required_fee) END AS donations_paid FROM (donations d JOIN v_donation_totals v ON ((d.id = v.id)));", :force => true do |v|
     v.column :id
     v.column :contact_id
     v.column :postal_code
