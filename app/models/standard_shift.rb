a5055ee0f7c39747598f7781e0c6329589cfa60f
Richard Seymour <rfs@dev.freegeek.org>
2008-02-08 17:40:36 +0000

    edit job page has links to edit shifts now

--- app/models/standard_shift.rb
+++ app/models/standard_shift.rb
@@ -18,4 +18,10 @@ class StandardShift < ActiveRecord::Base
     ret.gsub( ':00', '' ).gsub( ' 0', ' ').gsub( ' - ', '-' )
   end
 
+  def job_name
+    weekday = Weekday.find(:first, :conditions => "id = #{weekday_id}").short_name + ', ' 
+    ret = weekday + Worker.find(:first, :conditions => "id = #{worker_id}").name + ' ' + start_time.strftime("%I:%M") + ' - ' + end_time.strftime("%I:%M")
+    ret.gsub( ':00', '' ).gsub( ' 0', ' ').gsub( ' - ', '-' )
+  end
+
 end
