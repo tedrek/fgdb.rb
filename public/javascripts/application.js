d12aad04f1728283f5807b408e646c1ecf4591db
Martin Chase <stillflame@dev.freegeek.org>
2008-02-08 00:08:45 +0000

    - added new prototype.js - put back in some of the AjaxScaffold js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,45 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+var AjaxScaffold = {
+  stripe: function(tableBody) {
+    var even = false;
+    var tableBody = $(tableBody);
+    var tableRows = tableBody.getElementsByTagName("tr");
+    var length = tableBody.rows.length;
+
+    for (var i = 0; i < length; i++) {
+      var tableRow = tableBody.rows[i];
+      //Make sure to skip rows that are create or edit rows or messages
+      if (!Element.hasClassName(tableRow, "create")
+        && !Element.hasClassName(tableRow, "update")) {
+
+        if (even) {
+          Element.addClassName(tableRow, "even");
+        } else {
+          Element.removeClassName(tableRow, "even");
+        }
+        even = !even;
+      }
+    }
+  },
+  displayMessageIfEmpty: function(tableBody, emptyMessageElement) {
+    // Check to see if this was the last element in the list
+    if ($(tableBody).rows.length == 0) {
+      Element.show($(emptyMessageElement));
+    }
+  },
+  removeSortClasses: function(scaffoldId) {
+    $$('#' + scaffoldId + ' td.sorted').each(function(element) {
+      Element.removeClassName(element, "sorted");
+    });
+    $$('#' + scaffoldId + ' th.sorted').each(function(element) {
+      Element.removeClassName(element, "sorted");
+      Element.removeClassName(element, "asc");
+      Element.removeClassName(element, "desc");
+    });
+  }
+}
 // Called as:
 // confirmReplaceElementValue(elem_id, new_elem_value, confirm_message)
 function confirmReplaceElementValue(id, val, msg){
@@ -29,7 +68,7 @@ function calculateOffsetTop(r){
 function absolute_offset(r,attr){
   var tot=0;
   while(r){
-    tot+=r[attr]; 
+    tot+=r[attr];
     r=r.offsetParent
   }
   return tot
@@ -37,9 +76,9 @@ function absolute_offset(r,attr){
 
 function contactKeyListener(event) {
   var keyCode = event.keyCode ? event.keyCode : event.which ? event.which : event.charCode;
-  
-	if (keyCode == 13 || keyCode == 10) {
-		return false;
+
+        if (keyCode == 13 || keyCode == 10) {
+                return false;
   }
 }
 
