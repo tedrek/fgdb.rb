be28a21f502fe2fc0a5aecbd983f7004034bdefa
Lantz Moore <lmoore@dev.freegeek.org>
2008-02-20 03:02:11 +0000

    - added ajaxy login/logout bit to the global header

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,14 @@
 # This controller handles the login/logout function of the site.  
 class SessionsController < ApplicationController
-  # Be sure to include AuthenticationSystem in Application Controller instead
-  include AuthenticatedSystem
-
   # render new.rhtml
   def new
   end
 
   def create
     self.current_user = User.authenticate(params[:login], params[:password])
-    if logged_in?
-      if params[:remember_me] == "1"
-        self.current_user.remember_me
-        cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
-      end
-      redirect_back_or_default('/')
-      flash[:notice] = "Logged in successfully"
-    else
-      render :action => 'new'
+    flash[:error] = "invalid username/password" unless logged_in?
+    render :update do |page|
+      page.replace("login_box", :partial => "you_are_here")
     end
   end
 
@@ -25,8 +16,10 @@ class SessionsController < ApplicationController
     self.current_user.forget_me if logged_in?
     cookies.delete :auth_token
     reset_session
-    flash[:notice] = "You have been logged out."
-    redirect_back_or_default('/')
+    @current_user = nil
+    render :update do |page|
+      page.replace("login_box", :partial => "you_are_here")
+    end
   end
 
   def dump
