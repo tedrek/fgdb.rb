edca067e2eba6b09328e40e7b1709a647f761d17
Lantz Moore <lmoore@dev.freegeek.org>
2008-02-20 03:11:38 +0000

    - replace the sidebar links after login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,7 @@ class SessionsController < ApplicationController
   def create
     self.current_user = User.authenticate(params[:login], params[:password])
     flash[:error] = "invalid username/password" unless logged_in?
-    render :update do |page|
-      page.replace("login_box", :partial => "you_are_here")
-    end
+    rerender()
   end
 
   def destroy
@@ -17,12 +15,19 @@ class SessionsController < ApplicationController
     cookies.delete :auth_token
     reset_session
     @current_user = nil
-    render :update do |page|
-      page.replace("login_box", :partial => "you_are_here")
-    end
+    rerender()
   end
 
   def dump
     render :text => "<pre>#{session.to_yaml}</pre>"
+  end
+
+  protected
+
+  def rerender
+    render :update do |page|
+      page.replace("login_box", :partial => "you_are_here")
+      page.replace("sidebar", :partial => "sidebar_links/sidebar")
+    end
   end
 end
