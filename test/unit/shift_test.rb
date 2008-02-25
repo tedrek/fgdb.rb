9a6ae72a6d2edb9fa0ca5ba5dc0537ffe65facf2
Richard Seymour <rfs@dev.freegeek.org>
2008-02-25 19:35:52 +0000

    tests generated when supporting single table inheritance on shifts, meetings, unavailabilities

---
+++ test/unit/shift_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ShiftTest < Test::Unit::TestCase
+  fixtures :shifts
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
