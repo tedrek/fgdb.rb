49565b9d77f52228dbaec30ef557dc607baee2c3
Richard Seymour <rfs@dev.freegeek.org>
2008-02-28 16:49:22 +0000

    disabled verification on work_shifts (to more easily see bug). noted bug about sql referring to shifts when it should refer to work_shifts table

--- app/controllers/work_shifts_controller.rb
+++ app/controllers/work_shifts_controller.rb
@@ -5,8 +5,8 @@ class WorkShiftsController < ApplicationController
   end
 
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
+  #verify :method => :post, :only => [ :destroy, :create, :update ],
+  #       :redirect_to => { :action => :list }
 
   def list
     session["shift_return_to"] = "work_shifts"
