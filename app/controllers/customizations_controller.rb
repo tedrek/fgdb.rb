5543e7ce82df56a10b98004edd77f90561a7a725
Richard Seymour <rfs@dev.freegeek.org>
2008-03-06 16:04:08 +0000

    adding customizations scaffolding

---
+++ app/controllers/customizations_controller.rb
@@ -0,0 +1,51 @@
+class CustomizationsController < ApplicationController
+  def index
+    list
+    render :action => 'list'
+  end
+
+  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+  verify :method => :post, :only => [ :destroy, :create, :update ],
+         :redirect_to => { :action => :list }
+
+  def list
+    @customization_pages, @customizations = paginate :customizations, :per_page => 10
+  end
+
+  def show
+    @customization = Customization.find(params[:id])
+  end
+
+  def new
+    @customization = Customization.new
+  end
+
+  def create
+    @customization = Customization.new(params[:customization])
+    if @customization.save
+      flash[:notice] = 'Customization was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @customization = Customization.find(params[:id])
+  end
+
+  def update
+    @customization = Customization.find(params[:id])
+    if @customization.update_attributes(params[:customization])
+      flash[:notice] = 'Customization was successfully updated.'
+      redirect_to :action => 'show', :id => @customization
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    Customization.find(params[:id]).destroy
+    redirect_to :action => 'list'
+  end
+end
---
+++ test/unit/customization_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class CustomizationTest < Test::Unit::TestCase
+  fixtures :customizations
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
