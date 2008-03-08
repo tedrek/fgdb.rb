19fd5cdab5b2f430a07f25122df84b1097e51cab
Ryan Niebur <ryanryan52@gmail.com>
2008-03-08 08:42:22 +0000

    Tried to make the fix_null_date a little bit better

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,94 +6,36 @@ class ApplicationController < ActionController::Base
   session :session_key => '_fgsched_session_id'
   before_filter :fix_null_date
   skip_before_filter :fix_null_date, :except => ["update","create"]
-  
+
+  def fix_this_null_date (c1, c2)
+    if params[c1]
+      if params[c1][c2].empty?
+        params[c1][c2] = nil
+      end
+    end
+  end
+
   def fix_null_date
     # go ahead and laugh at this martin. i know there's a much
     # better way to do it
-    if params[:holiday]
-      if params[:holiday][:holiday_date].empty?
-        params[:holiday][:holiday_date] = nil
-      end
-    end
-    if params[:meeting]
-      if params[:meeting][:meeting_date]
-        if params[:meeting][:meeting_date].empty?
-          params[:meeting][:meeting_date] = nil
-        end
-      end
-      if params[:meeting][:effective_date]
-        if params[:meeting][:effective_date].empty?
-          params[:meeting][:effective_date] = nil
-        end
-      end
-      if params[:meeting][:ineffective_date]
-        if params[:meeting][:ineffective_date].empty?
-          params[:meeting][:ineffective_date] = nil
-        end
-      end
-    end
-    if params[:schedule]
-      if params[:schedule][:effective_date].empty?
-        params[:schedule][:effective_date] = nil
-      end
-      if params[:schedule][:ineffective_date].empty?
-        params[:schedule][:ineffective_date] = nil
-      end
-    end
-    if params[:shift]
-      if params[:shift][:shift_date].empty?
-        params[:shift][:shift_date] = nil
-      end
-      if params[:shift][:effective_date].empty?
-        params[:shift][:effective_date] = nil
-      end
-      if params[:shift][:ineffective_date].empty?
-        params[:shift][:ineffective_date] = nil
-      end
-    end
-    if params[:standard_shift]
-      if params[:standard_shift][:shift_date].empty?
-        params[:standard_shift][:shift_date] = nil
-      end
-    end
-    if params[:unavailability]
-      if params[:unavailability][:effective_date].empty?
-        params[:unavailability][:effective_date] = nil
-      end
-      if params[:unavailability][:ineffective_date].empty?
-        params[:unavailability][:ineffective_date] = nil
-      end
-    end
-    if params[:vacation]
-      if params[:vacation][:effective_date].empty?
-        params[:vacation][:effective_date] = nil
-      end
-      if params[:vacation][:ineffective_date].empty?
-        params[:vacation][:ineffective_date] = nil
-      end
-    end
-    if params[:work_shift]
-      if params[:work_shift][:shift_date].empty?
-        params[:work_shift][:shift_date] = nil
-      end
-      if params[:work_shift][:effective_date]
-        if params[:work_shift][:effective_date].empty?
-          params[:work_shift][:effective_date] = nil
-        end
-      end
-      if params[:work_shift][:ineffective_date]
-        if params[:work_shift][:ineffective_date].empty?
-          params[:work_shift][:ineffective_date] = nil
-        end
-      end
-    end
-    if params[:worker]
-      if params[:worker][:effective_date].empty?
-        params[:worker][:effective_date] = nil
-      end
-      if params[:worker][:ineffective_date].empty?
-        params[:worker][:ineffective_date] = nil
-      end
-    end
+    fix_this_null_date(:holliday, :holliday_date)
+    fix_this_null_date(:meeting, :meeting_date)
+    fix_this_null_date(:meeting, :effective_date)
+    fix_this_null_date(:meeting, :ineffective_date)
+    fix_this_null_date(:schedule, :effective_date)
+    fix_this_null_date(:schedule, :ineffective_date)
+    fix_this_null_date(:shift, :shift_date)
+    fix_this_null_date(:shift, :effective_date)
+    fix_this_null_date(:shift, :ineffective_date)
+    fix_this_null_date(:standard_shift, :shift_date)
+    fix_this_null_date(:unavailability, :effective_date)
+    fix_this_null_date(:unavailability, :ineffective_date)
+    fix_this_null_date(:vacation, :effective_date)
+    fix_this_null_date(:vacation, :ineffective_date)
+    fix_this_null_date(:work_shift, :shift_date)
+    fix_this_null_date(:work_shift, :effective_date)
+    fix_this_null_date(:work_shift, :ineffective_date)
+    fix_this_null_date(:worker, :effective_date)
+    fix_this_null_date(:worker, :ineffective_date)
   end
 end
