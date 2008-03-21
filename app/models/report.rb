076976e3ea3e4b0a8a95ae662a5b2c06b0d3b75d
Ryan Niebur <ryanryan52@gmail.com>
2008-03-21 19:53:31 +0000

    I fixed that in the views

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,12 +4,6 @@ class Report < ActiveRecord::Base
   belongs_to :system
   belongs_to :type
 
-  def type_id=(type)
-    write_attribute(:type_id, Type.find_by_name(type).id)
-  end
-  def role_id=(role)
-    write_attribute(:role_id, Role.find_by_name(role).id)
-  end
   def my_file=(file)
       write_attribute(:lshw_output, file.read)
   end
