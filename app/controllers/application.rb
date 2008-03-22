5d1d589165946560e32d1ab32a2581a367599ef1
Ryan Niebur <ryanryan52@gmail.com>
2008-03-22 16:49:48 +0000

    I dont care about protection

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,5 +6,5 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '7326a2f827b181147cd5036542a0855e'
+  #protect_from_forgery # :secret => '7326a2f827b181147cd5036542a0855e'
 end
