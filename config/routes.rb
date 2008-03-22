785a6a8e7a1e13691428e15e8f0bdaa269f51015
Ryan Niebur <ryanryan52@gmail.com>
2008-03-22 06:37:13 +0000

    For now just map them...just cause its getting anoying (ya...you were right Martin)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :systems
+  map.resources :roles
+  map.resources :types
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
