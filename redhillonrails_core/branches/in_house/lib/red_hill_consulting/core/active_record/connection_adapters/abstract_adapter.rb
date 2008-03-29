e3d664a1278ead1bad99d3514bc60152f5faa2f7
Ryan Niebur <ryanryan52@gmail.com>
2008-03-29 16:31:57 +0000

    Fixed redhill on rails to not delete stuff that violates the foreign key its adding. Now it will delete it if on_delete is restrict, cascade, or no action. It will set it to null if on_delete is set_null and it will set it to default if on_delete is set_default

--- redhillonrails_core/branches/in_house/lib/red_hill_consulting/core/active_record/connection_adapters/abstract_adapter.rb
+++ redhillonrails_core/branches/in_house/lib/red_hill_consulting/core/active_record/connection_adapters/abstract_adapter.rb
@@ -15,7 +15,13 @@ module RedHillConsulting::Core::ActiveRecord::ConnectionAdapters
     end
 
     def add_foreign_key(table_name, column_names, references_table_name, references_column_names, options = {})
-      execute "DELETE FROM #{table_name} WHERE #{column_names[0]} IS NOT Null AND NOT EXISTS (SELECT * FROM #{references_table_name} as rtn WHERE rtn.#{references_column_names[0]} = #{table_name}.#{column_names[0]})"
+      if options[:on_delete] == :set_null
+        execute "UPDATE #{table_name} SET #{column_names[0]} = NULL WHERE #{column_names[0]} IS NOT NULL AND NOT EXISTS (SELECT * FROM #{references_table_name} as rtn WHERE rtn.#{references_column_names[0]} = #{table_name}.#{column_names[0]})" 
+      elsif options[:on_delete] == :set_default
+        execute "UPDATE #{table_name} SET #{column_names[0]} = DEFAULT WHERE #{column_names[0]} IS NOT NULL AND NOT EXISTS (SELECT * FROM #{references_table_name} as rtn WHERE rtn.#{references_column_names[0]} = #{table_name}.#{column_names[0]})"
+      else #default behavior...do this for restrict, no action, and cascade
+      	execute "DELETE FROM #{table_name} WHERE #{column_names[0]} IS NOT NULL AND NOT EXISTS (SELECT * FROM #{references_table_name} as rtn WHERE rtn.#{references_column_names[0]} = #{table_name}.#{column_names[0]})"
+      end
       foreign_key = ForeignKeyDefinition.new(options[:name], table_name, column_names, ActiveRecord::Migrator.proper_table_name(references_table_name), references_column_names, options[:on_update], options[:on_delete], options[:deferrable])
       execute "ALTER TABLE #{table_name} ADD #{foreign_key}"
     end
