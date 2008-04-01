b54b25982a6520eaa2b50a5b8b19a4341af9c52e
Ryan Niebur <ryanryan52@gmail.com>
2008-04-01 01:43:57 +0000

    controller cleanup on roles and types

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,85 +1,39 @@
 class RolesController < ApplicationController
-  # GET /roles
-  # GET /roles.xml
   def index
-    @roles = Role.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @roles }
-    end
+    @types = Role.find(:all)
   end
 
-  # GET /roles/1
-  # GET /roles/1.xml
   def show
-    @role = Role.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @role }
-    end
+    @type = Role.find(params[:id])
   end
 
-  # GET /roles/new
-  # GET /roles/new.xml
   def new
-    @role = Role.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @role }
-    end
+    @type = Role.new
   end
 
-  # GET /roles/1/edit
   def edit
-    @role = Role.find(params[:id])
+    @type = Role.find(params[:id])
   end
 
-  # POST /roles
-  # POST /roles.xml
   def create
-    @role = Role.new(params[:role])
+    @type = type.new(params[:report])
 
-    respond_to do |format|
-      if @role.save
-        flash[:notice] = 'Role was successfully created.'
-        format.html { redirect_to(@role) }
-        format.xml  { render :xml => @role, :status => :created, :location => @role }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @role.errors, :status => :unprocessable_entity }
-      end
+    if @type.save
+      flash[:notice] = 'Role was successfully created.'
+      redirect_to(:action=>"show", :id=>@report.id)
+    else
+      render :action => "new", :error => "Could not save the database record"
     end
   end
 
-  # PUT /roles/1
-  # PUT /roles/1.xml
   def update
-    @role = Role.find(params[:id])
+    @type = Role.find(params[:id])
 
-    respond_to do |format|
-      if @role.update_attributes(params[:role])
-        flash[:notice] = 'Role was successfully updated.'
-        format.html { redirect_to(@role) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @role.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /roles/1
-  # DELETE /roles/1.xml
-  def destroy
-    @role = Role.find(params[:id])
-    @role.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(roles_url) }
-      format.xml  { head :ok }
+    if @type.update_attributes(params[:type])
+      flash[:notice] = 'Role was successfully created.'
+      redirect_to(:action=>"show", :id=>@report.id)
+    else
+      render :action => "new", :error => "Could not save the database record"
     end
   end
 end
--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,85 +1,39 @@
 class TypesController < ApplicationController
-  # GET /types
-  # GET /types.xml
   def index
     @types = Type.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @types }
-    end
   end
 
-  # GET /types/1
-  # GET /types/1.xml
   def show
     @type = Type.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @type }
-    end
   end
 
-  # GET /types/new
-  # GET /types/new.xml
   def new
     @type = Type.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @type }
-    end
   end
 
-  # GET /types/1/edit
   def edit
     @type = Type.find(params[:id])
   end
 
-  # POST /types
-  # POST /types.xml
   def create
-    @type = Type.new(params[:type])
+    @type = type.new(params[:report])
 
-    respond_to do |format|
-      if @type.save
-        flash[:notice] = 'Type was successfully created.'
-        format.html { redirect_to(@type) }
-        format.xml  { render :xml => @type, :status => :created, :location => @type }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @type.errors, :status => :unprocessable_entity }
-      end
+    if @type.save
+      flash[:notice] = 'Type was successfully created.'
+      redirect_to(:action=>"show", :id=>@report.id)
+    else
+      render :action => "new", :error => "Could not save the database record"
     end
   end
 
-  # PUT /types/1
-  # PUT /types/1.xml
   def update
     @type = Type.find(params[:id])
 
-    respond_to do |format|
-      if @type.update_attributes(params[:type])
-        flash[:notice] = 'Type was successfully updated.'
-        format.html { redirect_to(@type) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @type.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /types/1
-  # DELETE /types/1.xml
-  def destroy
-    @type = Type.find(params[:id])
-    @type.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(types_url) }
-      format.xml  { head :ok }
+    if @type.update_attributes(params[:type])
+      flash[:notice] = 'Type was successfully created.'
+      redirect_to(:action=>"show", :id=>@report.id)
+    else
+      render :action => "new", :error => "Could not save the database record"
     end
   end
 end
