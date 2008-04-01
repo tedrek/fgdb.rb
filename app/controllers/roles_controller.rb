8ffe7317aa6a54ad62f644f27032ef932165603a
Ryan Niebur <ryanryan52@gmail.com>
2008-04-01 01:46:36 +0000

    I need to be more careful when I copy and paste

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,24 +1,24 @@
 class RolesController < ApplicationController
   def index
-    @types = Role.find(:all)
+    @roles = Role.find(:all)
   end
 
   def show
-    @type = Role.find(params[:id])
+    @role = Role.find(params[:id])
   end
 
   def new
-    @type = Role.new
+    @role = Role.new
   end
 
   def edit
-    @type = Role.find(params[:id])
+    @role = Role.find(params[:id])
   end
 
   def create
-    @type = type.new(params[:report])
+    @role = role.new(params[:report])
 
-    if @type.save
+    if @role.save
       flash[:notice] = 'Role was successfully created.'
       redirect_to(:action=>"show", :id=>@report.id)
     else
@@ -27,9 +27,9 @@ class RolesController < ApplicationController
   end
 
   def update
-    @type = Role.find(params[:id])
+    @role = Role.find(params[:id])
 
-    if @type.update_attributes(params[:type])
+    if @role.update_attributes(params[:role])
       flash[:notice] = 'Role was successfully created.'
       redirect_to(:action=>"show", :id=>@report.id)
     else
