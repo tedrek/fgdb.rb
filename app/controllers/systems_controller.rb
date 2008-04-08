d257cce825d953a4a641fc29ef6daa7a90a46ae5
Ryan Niebur <ryanryan52@gmail.com>
2008-04-08 15:18:30 +0000

    We do use this

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -7,10 +7,10 @@ class SystemsController < ApplicationController
   def method_missing(symbol, *args)
     redirect_to :controller => "reports", :action => "index"
   end
-  #I don't think we need  this
-  #def xml_new
-  #  @system = System.new
-  #  @system.save
-  #  render :xml => @system
-  #end
+
+  def xml_new
+    @system = System.new
+    @system.save
+    render :xml => @system
+  end
 end
