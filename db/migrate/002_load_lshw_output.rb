c0054dbf608a889a8ad0f89869f3edaf65715530
Ryan Niebur <ryanryan52@gmail.com>
2008-04-09 04:25:21 +0000

    Made it work

--- db/migrate/002_load_lshw_output.rb
+++ db/migrate/002_load_lshw_output.rb
@@ -1,20 +1,17 @@
 class LoadLshwOutput < ActiveRecord::Migration
   def self.up
     for report in Report.find(:all)
-      file = "/var/www/fgss/lshw_xml/#{system_id.to_s}-#{role_id.to_s}-#{report.created_at.to_date.strftime("%Y-%m-%d-%H%M")}.xml" 
+      file = "/var/www/fgss/lshw_xml/#{report.system_id.to_s}-#{report.role_id.to_s}-#{report.created_at.to_time.strftime("%Y-%m-%d-%H%M")}.xml" 
       if File.exist?(file)
         fd = File.open(file, 'r')
         report.lshw_output = fd.read
         fd.close
       else
-        if report.lshw_output == nil || report.lshw_output == ""
-          raise "report ##{report.id} = no file + no date in db = bad!"
-        end
       end
     end
   end
 
   def self.down
-    raise "I don't know and I don't care!"
+    raise "I DONT CARE"
   end
 end
