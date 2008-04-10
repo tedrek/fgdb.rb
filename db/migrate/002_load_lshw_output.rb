0b1fcc9f2117e6507252bfc389ae6afda0309d86
Ryan Niebur <ryanryan52@gmail.com>
2008-04-10 14:53:22 +0000

    Made the xml migration work. and order by id so the "system." information has the most recent info.

--- db/migrate/002_load_lshw_output.rb
+++ db/migrate/002_load_lshw_output.rb
@@ -1,11 +1,12 @@
 class LoadLshwOutput < ActiveRecord::Migration
   def self.up
-    for report in Report.find(:all)
+    for report in Report.find(:all, :order => "id")
       file = "/var/www/fgss/lshw_xml/#{report.system_id.to_s}-#{report.role_id.to_s}-#{report.created_at.to_time.strftime("%Y-%m-%d-%H%M")}.xml" 
       if File.exist?(file)
         fd = File.open(file, 'r')
         report.lshw_output = fd.read
         fd.close
+	report.save
       else
       end
     end
