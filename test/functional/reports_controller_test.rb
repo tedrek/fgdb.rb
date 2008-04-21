c683a2237f7d513c52303463868da5289654ff41
Ryan Niebur <ryanryan52@gmail.com>
2008-04-21 18:54:17 +0000

    - more tests

---
+++ test/functional/reports_controller_test.rb
@@ -0,0 +1,40 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ReportsControllerTest < ActionController::TestCase
+  def test_that_xml_system_show_does_not_show_a_nonexistant_system
+    get :xml_system_show, {:id => "50"}
+    assert_response :redirect
+    assert_redirected_to :controller => 'reports', :action => 'xml_index', :error => "That system does not exist!"
+  end
+  
+  def test_that_xml_system_show_shows_a_system
+    get :xml_system_show, {:id => "1"}
+    assert_response :success
+  end
+
+  def test_that_bad_xml_files_fail
+    file = File.open(File.dirname(__FILE__) + "/../fixtures/bad.xml")
+    get :xml_create, :my_file => file
+    file.close
+    assert_response :redirect
+    assert_redirected_to :controller => 'reports', :action => "xml_index", :error => "Invalid XML!"
+  end
+
+  def test_that_good_xml_files_succeed
+    file = File.open(File.dirname(__FILE__) + "/../fixtures/1967.xml")
+    get :xml_create, :my_file => file
+    file.close
+    assert_response :redirect
+    assert_redirected_to :controller => 'reports', :action => "xml_show"
+  end
+
+  def test_that_version_compat_fails
+    get :check_compat, { :version => 1000 }
+    assert_tag :tag => "compat", :child => { :content => "true" }
+  end
+
+  def test_that_version_compat_succeeds
+    get :check_compat, { :version => 0 }
+    assert_tag :tag => "compat", :child => { :content => "false" }
+  end
+end
--- test/unit/report_test.rb
+++ test/unit/report_test.rb
@@ -1,13 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  fixtures :reports, :systems
-
-  def setup
-    Report.connection.execute "DELETE FROM reports"
-    System.connection.execute "DELETE FROM systems"
-  end
-
   def test_that_generics_are_detected(stop = 0)
     file = File.open(File.dirname(__FILE__) + "/../fixtures/1967.xml")
     report = Report.new(:lshw_output => file.read)
@@ -41,5 +34,34 @@ class ReportTest < ActiveSupport::TestCase
     assert_nothing_raised {report1.system.id}
     assert_nothing_raised {report2.system.id}
     assert_equal report1.system.id, report2.system.id
+  end
+
+  def test_that_different_xml_files_produce_different_systems
+    file1 = File.open(File.dirname(__FILE__) + "/../fixtures/1967.xml")
+    data1 = file1.read
+    file1.close
+    file2 = File.open(File.dirname(__FILE__) + "/../fixtures/1967_different_vendor_model.xml")
+    data2 = file2.read
+    file2.close
+    file3 = File.open(File.dirname(__FILE__) + "/../fixtures/1967.xml")
+    data3 = file3.read
+    file3.close
+    file4 = File.open(File.dirname(__FILE__) + "/../fixtures/1967_different_serial.xml")
+    data4 = file4.read
+    file4.close
+    report1 = Report.new(:lshw_output => data1)
+    report1.save
+    report2 = Report.new(:lshw_output => data2)
+    report2.save
+    report3 = Report.new(:lshw_output => data3)
+    report3.save
+    report4 = Report.new(:lshw_output => data4)
+    report4.save
+    assert_equal report1.system.id, report3.system.id
+    assert_not_equal report1.system.id, report2.system.id
+    assert_not_equal report3.system.id, report4.system.id
+    assert_not_equal report2.system.id, report4.system.id
+    assert_not_equal report1.system.id, report4.system.id
+    assert_not_equal report2.system.id, report3.system.id
   end
 end
