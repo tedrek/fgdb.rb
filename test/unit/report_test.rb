03a3ba2232787be80f19a984c116245e4c31745f
Ryan Niebur <ryanryan52@gmail.com>
2008-04-21 22:00:59 +0000

    - took out some assertions that are pointless

--- test/unit/report_test.rb
+++ test/unit/report_test.rb
@@ -23,14 +23,7 @@ class ReportTest < ActiveSupport::TestCase
     report1 = Report.new(:lshw_output => data)
     report1.save
     report2 = Report.new(:lshw_output => data)
-    assert report2.get_serial != "(no serial number)"
-    assert report2.get_serial == report1.get_serial
-    assert_equal 1, System.find_all_by_serial_number(report2.get_serial).length 
-    assert_equal report1.system.id, report2.system.id
     report2.save
-    assert_equal report1.get_serial, report2.get_serial
-    assert_equal System.find_all_by_serial_number(report1.get_serial)[0].id, System.find_all_by_serial_number(report2.get_serial)[0].id
-    assert_equal System.find_all_by_serial_number(report1.get_serial).length, System.find_all_by_serial_number(report2.get_serial).length
     assert_nothing_raised {report1.system.id}
     assert_nothing_raised {report2.system.id}
     assert_equal report1.system.id, report2.system.id
