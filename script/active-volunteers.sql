04f869afddb2945b09d9b67ad14eaa184222a7c9
Richard Seymour <rfs@dev.freegeek.org>
2008-07-24 17:52:20 +0000

    added comment

--- script/active-volunteers.sql
+++ script/active-volunteers.sql
@@ -4,6 +4,10 @@
 
 -- NOTE: this doesn't pull every day -- only the days on which
 -- work was performed
+
+-- NOTE: the second column is a sub-query that returns only one
+-- record and one field. if it gets tampered with it must only
+-- return one record and one field or the overall query will fail
 SELECT 
   date_performed, 
   (SELECT count( distinct zzz.contact_id )
@@ -15,7 +19,7 @@ SELECT
         volunteer_tasks.date_performed - 90 AND 
         volunteer_tasks.date_performed 
       GROUP BY xxx.contact_id 
-      HAVING SUM(xxx.duration) > 4)) AS yyy 
+      HAVING SUM(xxx.duration) > 4)) AS vol_count 
   FROM volunteer_tasks 
   GROUP BY date_performed 
   ORDER BY 1;
