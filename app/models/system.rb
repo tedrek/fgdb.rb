ddfb23d2673dcc13da0eff73e07cd0119739609f
Ryan Niebur <ryanryan52@gmail.com>
2008-07-28 20:29:37 +0000

    typo

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,5 +1,5 @@
 class System < ActiveRecord::Base
   attr_protected :mobo_vendor, :system_vendor, :mobo_serial_number, :system_serial_number, :mobo_model, :system_model, :vendor, :serial_number, :model
   attr_readonly :mobo_vendor, :system_vendor, :mobo_serial_number, :system_serial_number, :mobo_model, :system_model, :vendor, :serial_number, :model
-  validate_presence_of :vendor, :serial_number, :model
+  validates_presence_of :vendor, :serial_number, :model
 end
