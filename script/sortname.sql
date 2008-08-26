ab7f386fe04ec60fe74c2da7dce024e61b1a0d24
Ryan Niebur <ryanryan52@gmail.com>
2008-08-26 20:37:39 +0000

    fixed the sql, I think. Added trigger

--- script/sortname.sql
+++ script/sortname.sql
@@ -1,17 +1,14 @@
--- Name: get_sort_name(character varying, character varying, character varying, character varying, character varying); Type: FUNCTION; Schema: public; Owner: rfs
---
-
 CREATE FUNCTION get_sort_name(character varying, character varying, character varying, character varying, character varying) RETURNS character varying
     AS '
 DECLARE
-    CONTACT_TYPE ALIAS FOR $1 ;
+    IS_ORG ALIAS FOR $1 ;
     FIRST_NAME ALIAS FOR $2 ;
     MIDDLE_NAME ALIAS FOR $3 ;
     LAST_NAME ALIAS FOR $4 ;
     ORG_NAME ALIAS FOR $5 ;
 
 BEGIN
-    IF CONTACT_TYPE = ''P'' THEN
+    IF IS_ORG = f THEN
        RETURN
          SUBSTR( TRIM( UPPER( 
            COALESCE(TRIM(LAST_NAME), '''') || 
@@ -33,12 +30,18 @@ END;
     LANGUAGE plpgsql;
 
 
---
--- TOC entry 306 (OID 2319022)
--- Name: get_sort_name(character varying, character varying, character varying, character varying, character varying); Type: ACL; Schema: public; Owner: rfs
---
+CREATE FUNCTION contact_trigger() RETURNS "trigger"
+    AS '
+BEGIN
+    NEW.sortname := get_sort_name(NEW.is_organization, NEW.first_name, NEW.middle_name, NEW.last_name, NEW.organization);
+    RETURN NEW;
+END;
+'
+    LANGUAGE plpgsql;
 
-REVOKE ALL ON FUNCTION get_sort_name(character varying, character varying, character varying, character varying, character varying) FROM PUBLIC;
-REVOKE ALL ON FUNCTION get_sort_name(character varying, character varying, character varying, character varying, character varying) FROM rfs;
-GRANT ALL ON FUNCTION get_sort_name(character varying, character varying, character varying, character varying, character varying) TO PUBLIC;
+
+CREATE TRIGGER contact_addr_insert_trigger
+    BEFORE INSERT OR UPDATE ON contact
+    FOR EACH ROW
+    EXECUTE PROCEDURE contact_trigger();
 
