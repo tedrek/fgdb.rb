e782b68c763a2d8375cab46ae2119f3eb8cb9f0b
Ryan Niebur <ryanryan52@gmail.com>
2008-09-12 05:09:40 +0000

    made the migration better

--- db/migrate/20080912040238_keep_orig_lshw_xml_too.rb
+++ db/migrate/20080912040238_keep_orig_lshw_xml_too.rb
@@ -6,13 +6,13 @@ class KeepOrigLshwXmlToo < ActiveRecord::Migration
     add_column "spec_sheets", :cleaned_valid, :boolean
     add_column "spec_sheets", :original_valid, :boolean
     SpecSheet.connection.execute("UPDATE spec_sheets SET original_output=lshw_output")
-    SpecSheet.connection.execute("UPDATE spec_sheets SET cleaned_output=lshw_output")
-    SpecSheet.find(:all).each{|x|
-      x.original_valid = (load_xml(x.original_output) ? true : false) # SLOW!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      x.save
-    }
-    SpecSheet.connection.execute("UPDATE spec_sheets SET cleaned_valid=original_valid") # original and cleaned are the same, don't waste all of the time needed to load all of that xml again
     remove_column "spec_sheets", :lshw_output
+    SpecSheet.find(:all).each{|x|
+      if x.original_output
+        x.lshw_output = x.original_output
+        x.save!
+      end
+    }
   end
 
   def self.down
