c5cd5807b74eca147b02a4071650ee1c8cb3fc77
Lantz Moore <lmoore@dev.freegeek.org>
2008-09-21 01:49:44 +0000

    * order by count and dup_check app

--- app/models/contact_duplicate.rb
+++ app/models/contact_duplicate.rb
@@ -3,10 +3,10 @@ class ContactDuplicate < ActiveRecord::Base
 
   def self.list_dups()
     dups = self.connection.execute("
-      SELECT dup_check, count(*)
+      SELECT dup_check, count(*) as count
       FROM contact_duplicates
       GROUP BY dup_check
-      ORDER BY dup_check
+      ORDER BY count DESC, dup_check
     ").map{|x| [x["dup_check"],x["count"]]}
 
     return dups
