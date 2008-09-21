bd85ab2c4f467c979b024c4dbab695b81879d813
Ryan Niebur <ryanryan52@gmail.com>
2008-09-21 01:13:17 +0000

    save more stuff

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -22,6 +22,13 @@ class Contact < ActiveRecord::Base
       connection.execute("UPDATE donations SET contact_id = #{self.id} WHERE contact_id = #{other.id}")
       connection.execute("UPDATE sales SET contact_id = #{self.id} WHERE contact_id = #{other.id}")
       connection.execute("UPDATE disbursements SET contact_id = #{self.id} WHERE contact_id = #{other.id}")
+      connection.execute("UPDATE contacts SET created_at = (SELECT min(created_at) FROM contacts WHERE id IN (#{self.id}, #{other.id}))")
+      self.notes = [self.notes, other.notes].delete_if{|x| x.nil?}.join("\n")
+      self.save!
+      connection.execute("UPDATE contacts SET updated_at = (SELECT max(updated_at) FROM contacts WHERE id IN (#{self.id}, #{other.id}))")
+      connection.execute("UPDATE contact_methods SET contact_id = #{self.id} WHERE contact_id = #{other.id}")
+      self.contact_types = (self.contact_types + other.contact_types).uniq
+      self.save!
       if self.contact_duplicate
         ContactDuplicate.delete_all(:dup_check => self.contact_duplicate.dup_check)
       end
