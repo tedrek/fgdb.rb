fe62df1e6e6da7d906a3188618aad8e23f8cc731
Ryan Niebur <ryanryan52@gmail.com>
2008-10-11 04:58:45 +0000

    televisions don't change

--- db/migrate/20081011033125_change_fee_for_crts.rb
+++ db/migrate/20081011033125_change_fee_for_crts.rb
@@ -1,10 +1,9 @@
 class ChangeFeeForCrts < ActiveRecord::Migration
   def self.up
-    GizmoType.connection.execute("UPDATE gizmo_types SET description='Old Fee Television', name='old_television' WHERE description='Television'")
     GizmoType.connection.execute("UPDATE gizmo_types SET description='Old Fee CRT', name='old_crt' WHERE description='CRT'")
     GizmoType.connection.execute("UPDATE gizmo_types SET description='Old Fee Sys w/ monitor', name='old_sys_with_monitor' WHERE description='Sys w/ monitor'")
     parent = GizmoType.find_by_description("Monitor")
-    {:television => ["Television", 4], :sys_with_monitor => ["Sys w/ monitor", 1], :crt => ["CRT", 2]}.each do |k,v|
+    {:sys_with_monitor => ["Sys w/ monitor", 1], :crt => ["CRT", 2]}.each do |k,v|
       gt = GizmoType.new(:name => k.to_s, :description => v[0], :parent => parent, :gizmo_contexts => GizmoContext.find(:all).to_a, :gizmo_category_id => v[1], :suggested_fee_cents => 0, :required_fee_cents => 700)
       gt.save!
       DiscountSchedule.find(:all).to_a.map{|x| x.id}.each{|x|
