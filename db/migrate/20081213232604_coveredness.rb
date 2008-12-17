0f51b5b8b7af0e9b0cc7d77bcdac092aacf17dc7
Ryan Niebur <ryanryan52@gmail.com>
2008-12-16 20:05:12 -0800

    fix migration

--- db/migrate/20081213232604_coveredness.rb
+++ db/migrate/20081213232604_coveredness.rb
@@ -1,8 +1,8 @@
 class Coveredness < ActiveRecord::Migration
   def self.up
-    add_column :contacts, :fully_covered
-    add_column :gizmo_types, :covered
-    add_column :gizmo_events, :covered
+    add_column :contacts, :fully_covered, :boolean
+    add_column :gizmo_types, :covered, :boolean
+    add_column :gizmo_events, :covered, :boolean
     for i in [:coveredness_enabled, :fully_covered_contact_covered_gizmo, :fully_covered_contact_uncovered_gizmo, :unfully_covered_contact_covered_gizmo, :unfully_covered_contact_uncovered_gizmo]
       d = Default.new
       d.name = i.to_s
