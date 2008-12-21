b77451f850383d2c7cf8623070b817e644edd14f
Ryan Niebur <ryanryan52@gmail.com>
2008-12-21 01:59:39 -0800

    use is-pdx

--- db/migrate/20081214003920_some_metadata_changes_richard_made.rb
+++ db/migrate/20081214003920_some_metadata_changes_richard_made.rb
@@ -1,13 +1,13 @@
 class SomeMetadataChangesRichardMade < ActiveRecord::Migration
   def self.up
-    # #573
+    # #573 (may be used in code someday)
     if !ContactType.find_by_name("nomail")
       ct = ContactType.new
       ct.name = "nomail"
       ct.description = "no mail"
       ct.save!
     end
-    # #574
+    # #574 (everybody wants generics! :D)
     for i in ["V        H 8\\", "VH\\"]
       if !Generic.find_by_value(i)
         g = Generic.new
@@ -15,18 +15,17 @@ class SomeMetadataChangesRichardMade < ActiveRecord::Migration
         g.save!
       end
     end
-    # #547
-    begin
-      remove_table :backuptable
-    rescue
-    end
-    # #579
-    if !VolunteerTaskType.find_by_name('case management')
-      vt = VolunteerTaskType.new
-      vt.name = 'case management'
-      vt.description = 'case management'
-      vt.parent_id = 24
-      vt.save!
+    if Default["is-pdx"] == "true"
+      # #547
+      remove_table :backuptable # our weird stuff
+      # #579
+      if !VolunteerTaskType.find_by_name('case management') # our stuff
+        vt = VolunteerTaskType.new
+        vt.name = 'case management'
+        vt.description = 'case management'
+        vt.parent_id = 24
+        vt.save!
+      end
     end
   end
 
