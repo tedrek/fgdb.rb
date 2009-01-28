f9ab9cca4493855ddee2e7b29c41bb3c4a8ed813
Ryan Niebur <ryanryan52@gmail.com>
2009-01-27 22:00:19 -0800

    order the contact types alphabetically

--- app/models/contact_type.rb
+++ app/models/contact_type.rb
@@ -2,7 +2,7 @@ class ContactType < ActiveRecord::Base
   validates_uniqueness_of :name, :message => "already exists"
 
   def self.find_instantiable
-    find(:all, :conditions => ["instantiable = ?", true])
+    find(:all, :conditions => ["instantiable = ?", true], :order => "description")
   end
 
   def to_s
