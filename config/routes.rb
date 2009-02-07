7d7c28c16d375ba960a737280339e7c58ac5b3fc
Ryan Niebur <ryanryan52@gmail.com>
2009-02-06 21:25:37 -0800

    map /staffsched

--- config/routes.rb
+++ config/routes.rb
@@ -45,4 +45,5 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.connect 'staffsched', :controller => "work_shifts", :action => "staffsched"
 end
