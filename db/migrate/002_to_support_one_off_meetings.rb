5e885290b82e0c66967ff0da5caea320c8f59133
Ryan Niebur <ryanryan52@gmail.com>
2009-02-06 21:18:22 -0800

    remove migrations

--- db/migrate/002_to_support_one_off_meetings.rb
+++
@@ -1,9 +0,0 @@
-class ToSupportOneOffMeetings < ActiveRecord::Migration
-  def self.up
-    add_column "standard_shifts", "shift_date", :date
-  end
-
-  def self.down
-    remove_column "standard_shifts", "shift_date"
-  end
-end
--- db/migrate/010_no_type_on_work_shift.rb
+++
@@ -1,9 +0,0 @@
-class NoTypeOnWorkShift < ActiveRecord::Migration
-  def self.up
-    rename_column "work_shifts", :type, :kind
-  end
-
-  def self.down
-    rename_column "work_shifts", :kind, :type
-  end
-end
