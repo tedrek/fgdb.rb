0dfd8457060e76a7608ae0b98c3a01fc161f4837
Ryan Niebur <ryanryan52@gmail.com>
2009-08-22 14:09:03 -0700

    add effective dates for volunteer task types too

--- app/models/volunteer_task_type.rb
+++ app/models/volunteer_task_type.rb
@@ -11,9 +11,9 @@ class VolunteerTaskType < ActiveRecord::Base
 
   named_scope :instantiables, { :conditions => {'instantiable' => true} }
 
-  def self.instantiable_nodes
-    self.instantiables.sort_by{|x| x.description}
-  end
+  named_scope :effective_on, lambda { |date|
+    { :conditions => ['(effective_on IS NULL OR effective_on < ?) AND (ineffective_on IS NULL OR ineffective_on > ?)', date, date] }
+  }
 
   def self.find_actual(*ids)
     ids.delete_if {|id| id == 0 }
