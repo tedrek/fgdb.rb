f4847973e37535248a52d6fbb25106ccb6964b1f
Ryan Niebur <ryanryan52@gmail.com>
2009-08-28 12:51:12 -0700

    make the form use the new can_login

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
 
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :password, :password_confirmation
+  attr_accessible :login, :email, :password, :password_confirmation, :can_login
 
   def self.reset_all_cashier_codes
     self.find(:all).each{|x|
