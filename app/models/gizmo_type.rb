c910babdfec491df3c7bacfab97e143ffb6def67
Ryan Niebur <ryanryan52@gmail.com>
2009-09-12 15:42:11 -0700

    fix the SQL, remove the debug statement, it works!

--- app/models/gizmo_type.rb
+++ app/models/gizmo_type.rb
@@ -14,7 +14,7 @@ class GizmoType < ActiveRecord::Base
   define_amount_methods_on("suggested_fee")
 
   named_scope :effective_on, lambda { |date|
-    { :conditions => ['(effective_on IS NULL OR effective_on < ?) AND (ineffective_on IS NULL OR ineffective_on > ?)', date, date] }
+    { :conditions => ['(effective_on IS NULL OR effective_on <= ?) AND (ineffective_on IS NULL OR ineffective_on > ?)', date, date] }
   }
 
   def GizmoType.fee?(type)
--- app/models/volunteer_task_type.rb
+++ app/models/volunteer_task_type.rb
@@ -12,7 +12,7 @@ class VolunteerTaskType < ActiveRecord::Base
   named_scope :instantiables, { :conditions => {'instantiable' => true} }
 
   named_scope :effective_on, lambda { |date|
-    { :conditions => ['(effective_on IS NULL OR effective_on < ?) AND (ineffective_on IS NULL OR ineffective_on > ?)', date, date] }
+    { :conditions => ['(effective_on IS NULL OR effective_on <= ?) AND (ineffective_on IS NULL OR ineffective_on > ?)', date, date] }
   }
 
   def self.find_actual(*ids)
