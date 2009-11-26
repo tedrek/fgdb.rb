c2feaef81346a916426325bfeb2603497103f4d2
Ryan Niebur <ryanryan52@gmail.com>
2009-11-26 13:41:53 -0800

    remove db/migrate/20091010234527_track_covered_on_printme.rb, don't want that for this release

--- db/migrate/20091010234527_track_covered_on_printme.rb
+++
@@ -1,9 +0,0 @@
-class TrackCoveredOnPrintme < ActiveRecord::Migration
-  def self.up
-    add_column :systems, :covered, :boolean
-  end
-
-  def self.down
-    remove_column :systems, :covered
-  end
-end
