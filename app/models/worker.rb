5ed7b535df0fea8fbda735e7b57e13d1a8eb8c97
Ryan Niebur <ryanryan52@gmail.com>
2009-12-12 00:32:52 -0800

    fix workers listing with a hack (closes: 16277)

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -51,6 +51,10 @@ class Worker < ActiveRecord::Base
     self.workers_worker_types.effective_on(date).first.worker_type
   end
 
+  def worker_type # FIXME: remove this
+    worker_type_today
+  end
+
   def worker_type_today
     self.worker_type_on_day(Date.today)
   end
