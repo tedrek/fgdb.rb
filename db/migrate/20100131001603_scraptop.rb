486696d80a996e002f7faad23ca6d9818792ddbf
Ryan Niebur <ryanryan52@gmail.com>
2010-01-30 16:21:10 -0800

    add scraptop gizmo type (Closes: #16647)

---
+++ db/migrate/20100131001603_scraptop.rb
@@ -0,0 +1,18 @@
+class Scraptop < ActiveRecord::Migration
+  def self.up
+      new = GizmoType.new
+      new.description = "Scraptop--AS-IS"
+      new.required_fee_cents = 0
+      new.suggested_fee_cents = 0
+      new.gizmo_category_id = 4
+      new.name = "scraptop"
+      new.covered = false
+      new.rank = 99
+      new.parent_name = "gizmo"
+      new.gizmo_contexts = [GizmoContext.sale]
+      new.save!
+  end
+
+  def self.down
+  end
+end
