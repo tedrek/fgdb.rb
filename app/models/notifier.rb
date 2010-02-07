81d8cceef8c52be8b7a832a48cd320c2c184966c
Ryan Niebur <ryanryan52@gmail.com>
2010-02-06 17:15:10 -0800

    send out a email to people who need to log their hours (Closes: #17032)

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -5,4 +5,19 @@ class Notifier < ActionMailer::Base
     subject "Volunteer Milestone Report"
     body :volunteers => volunteers
   end
+
+  def staff_hours_summary_report(myworkers)
+    recipients Default['scheduler_reports_to']
+    from Default['my_email_address']
+    subject "Staff Hours Summary"
+    body :myworkers => myworkers
+  end
+
+  def staff_hours_poke(myworker)
+    recipients myworker.name + " <" + myworker.email + ">"
+    from Default['my_email_address']
+    reply_to Default['scheduler_reports_to']
+    subject "Please log your hours"
+    body :myworker => myworker
+  end
 end
