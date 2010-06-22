0754af09b14e9bca011af50d5f83d8f2e1f5fd40
Ryan Niebur <ryanryan52@gmail.com>
2010-06-22 12:13:37 -0700

    UPDATE with FROM is slightly unintuitive. fix my failed syntax.

--- db/migrate/20100622184857_clean_up_covered.rb
+++ db/migrate/20100622184857_clean_up_covered.rb
@@ -4,15 +4,15 @@ class CleanUpCovered < ActiveRecord::Migration
              SET covered = 'f'
              WHERE contract_id = 2;")
     DB.exec("UPDATE gizmo_events
+             SET covered = 'f'
              FROM systems
-             SET gizmo_events.covered = 'f'
              WHERE systems.id = gizmo_events.system_id
              AND system_id IS NOT NULL
              AND systems.contract_id = 2
              AND gizmo_events.covered = 't';")
     DB.exec("UPDATE gizmo_events
+             SET covered = 'f'
              FROM donations
-             SET gizmo_events.covered = 'f'
              WHERE donation_id IS NOT NULL
              AND donations.id = gizmo_events.donation_id
              AND donations.contract_id != 1;")
