3faeda2915d4a116af5dff871248906d32da69bb
Ryan Niebur <ryanryan52@gmail.com>
2010-11-06 16:24:56 -0700

    fix displaying all airports on printme (Closes: #19143)

--- app/helpers/spec_sheets_helper.rb
+++ app/helpers/spec_sheets_helper.rb
@@ -3,7 +3,7 @@ module SpecSheetsHelper
     my_id = @parser.my_node 
     my_ser = @parser.xml_value_of("serial")
     my_ser = nil if my_ser == "Unknown"
-    if @seen.include?(my_id) && (my_ser && @seen_ser.include?(my_ser))
+    if @seen.include?(my_id) || (my_ser && @seen_ser.include?(my_ser))
       return ""
     else
       @seen << my_id
