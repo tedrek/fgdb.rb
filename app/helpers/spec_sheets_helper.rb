9566354cc2cf706e32793d614f8f55842bc93393
Ryan Niebur <ryanryan52@gmail.com>
2010-11-06 16:14:46 -0700

    fix showing some missing airport stuff from printme (Closes: #19143)

---
+++ app/helpers/spec_sheets_helper.rb
@@ -0,0 +1,14 @@
+module SpecSheetsHelper
+  def render_pci_guts()
+    my_id = @parser.my_node 
+    my_ser = @parser.xml_value_of("serial")
+    my_ser = nil if my_ser == "Unknown"
+    if @seen.include?(my_id) && (my_ser && @seen_ser.include?(my_ser))
+      return ""
+    else
+      @seen << my_id
+      @seen_ser << my_ser if my_ser
+    end
+    return render :partial => "pci_guts"
+  end
+end
