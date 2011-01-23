68f3a3838f0f453d08a3a1a70f4d1689621dec3c
Ryan Niebur <ryanryan52@gmail.com>
2011-01-22 17:46:57 -0800

    add a roster id to resources links

--- db/migrate/20110105165426_create_resources_volunteer_default_events.rb
+++ db/migrate/20110105165426_create_resources_volunteer_default_events.rb
@@ -5,10 +5,12 @@ class CreateResourcesVolunteerDefaultEvents < ActiveRecord::Migration
       t.integer :resource_id
       t.time :start_time
       t.time :end_time
+      t.integer :roster_id
 
       t.timestamps
     end
 
+    add_foreign_key "resources_volunteer_default_events", ["roster_id"], "rosters", ["id"], :on_delete => :restrict
     add_foreign_key "resources_volunteer_default_events", ["volunteer_default_event_id"], "volunteer_default_events", ["id"], :on_delete => :cascade
     add_foreign_key "resources_volunteer_default_events", ["resource_id"], "volunteer_events", ["id"], :on_delete => :restrict
   end
--- db/migrate/20110105165429_create_resources_volunteer_events.rb
+++ db/migrate/20110105165429_create_resources_volunteer_events.rb
@@ -6,11 +6,13 @@ class CreateResourcesVolunteerEvents < ActiveRecord::Migration
       t.integer :resources_volunteer_default_event_id
       t.time :start_time
       t.time :end_time
+      t.integer :roster_id
 
       t.timestamps
     end
 
-    add_foreign_key "resources_volunteer_events", ["resource_volunteer_default_event_id"], "resources_volunteer_default_events", ["id"], :on_delete => :set_null
+    add_foreign_key "resources_volunteer_events", ["roster_id"], "rosters", ["id"], :on_delete => :restrict
+    add_foreign_key "resources_volunteer_events", ["resources_volunteer_default_event_id"], "resources_volunteer_default_events", ["id"], :on_delete => :set_null
 
     add_foreign_key "resources_volunteer_events", ["volunteer_event_id"], "volunteer_events", ["id"], :on_delete => :cascade
     add_foreign_key "resources_volunteer_events", ["resource_id"], "volunteer_events", ["id"], :on_delete => :restrict
