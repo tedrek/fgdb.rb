580dace948c94c858cb8f711b842c989e09dbd4a
Ryan Niebur <ryanryan52@gmail.com>
2011-02-19 15:28:03 -0800

    actually save shifts

--- app/controllers/volunteer_events_controller.rb
+++ app/controllers/volunteer_events_controller.rb
@@ -20,7 +20,9 @@ class VolunteerEventsController < ApplicationController
   def create
     @volunteer_event = VolunteerEvent.new(params[:volunteer_event])
 
+    _save
     if @volunteer_event.save
+      _after_save
       flash[:notice] = 'VolunteerEvent was successfully created.'
       redirect_to({:action => "show", :id => @volunteer_event.id})
     else
@@ -31,12 +33,22 @@ class VolunteerEventsController < ApplicationController
   def update
     @volunteer_event = VolunteerEvent.find(params[:id])
 
+    _save
     if @volunteer_event.update_attributes(params[:volunteer_event])
+      _after_save
       flash[:notice] = 'VolunteerEvent was successfully updated.'
       redirect_to({:action => "show", :id => @volunteer_event.id})
     else
       render :action => "edit"
     end
+  end
+
+  def _save
+    @volunteer_shifts = apply_line_item_data(@volunteer_event, VolunteerShift)
+  end
+
+  def _after_save
+    @volunteer_shifts.each{|x| x.save!}
   end
 
   def destroy
