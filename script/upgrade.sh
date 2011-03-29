32042ead1987bd7e3838d4b1fdcd8e26b7bbf0ee
Ryan Niebur <ryanryan52@gmail.com>
2011-03-28 17:56:22 -0700

    switch from wayback to peabody

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+BACKUP_DIR=~/nobackup/db-backups/
+
+if ! test -f $BACKUP_DIR/I_AM_THE_BACKUPS; then
+    echo "ERROR: $BACKUP_DIR isn't the backup dir"
+    exit 1
+fi
+
+
 set -C
 set -e
 set -u
@@ -16,7 +24,7 @@ sudo git fetch origin
 sudo git checkout db/schema.rb
 sudo git checkout -b release_1.0.$NEW origin/release_1.0.$NEW
 sudo env RAILS_ENV=production rake db:migrate
-pg_dump fgdb_production > ~/nobackup/post-sprint-$NEW.sql
+pg_dump fgdb_production > $BACKUP_DIR/post-sprint-$NEW.sql
 
 sudo invoke-rc.d thin start
 sudo mv public/release.html public/_release.html
