2862722cc729478b28a1c2dacfe49afdf2ceb76a
Ryan Niebur <ryanryan52@gmail.com>
2011-04-01 19:37:41 -0700

    fix syntax

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -176,7 +176,7 @@ class Sale < ActiveRecord::Base
       gizmo_events << GizmoEvent.new({:unit_price_cents => storecredit_back,
                             :gizmo_count => 1,
                             :gizmo_type => GizmoType.find_by_name("store_credit"),
-                            :gizmo_context => self.gizmo_context)
+                                       :gizmo_context => self.gizmo_context})
 # <Ryan52> so it should just recalculate the expire date always? <store> yeah, that's it
 #                            :expire_date => store_credits_spent.map{|x| x.store_credit.expire_date}.uniq.select{|x| !x.nil?}.sort.last}) # WTF? something sets gizmo_context on *everything* else. why doesn't it set it on this one? hm...I can't find that code anyway.
     end
