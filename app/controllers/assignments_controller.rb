3fd5c7e3ffa9c30d4654d875c7f855dec19e1ceb
Ryan Niebur <ryanryan52@gmail.com>
2011-04-09 12:06:44 -0700

    line between tasks of different types

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -52,6 +52,7 @@ class AssignmentsController < ApplicationController
                                :left_table_name => "volunteer_shifts",
                                :left_link_action => "assign",
                                :left_link_id => "volunteer_shifts.description_and_slot",
+                               :break_between_difference => "assignments.slot_type_desc",
 
                                :thing_start_time => "assignments.start_time",
                                :thing_end_time => "assignments.end_time",
@@ -68,6 +69,7 @@ class AssignmentsController < ApplicationController
                                :left_table_name => "volunteer_shifts",
                                :left_link_action => "assign",
                                :left_link_id => "volunteer_shifts.description_and_slot",
+                               :break_between_difference => "assignment.slot_type_desc",
 
                                :thing_start_time => "assignments.start_time",
                                :thing_end_time => "assignments.end_time",
