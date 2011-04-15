43fa3234b39c44660aef0447061efa9f58af1d70
Ryan Niebur <ryanryan52@gmail.com>
2011-04-15 15:23:59 -0700

    uhh actually fix it I hope...I wasn't paying attention to the code

--- db/migrate/20110409192625_add_time_fields_to_default_assignments.rb
+++ db/migrate/20110409192625_add_time_fields_to_default_assignments.rb
@@ -4,12 +4,18 @@ class AddTimeFieldsToDefaultAssignments < ActiveRecord::Migration
     add_column :default_assignments, :end_time, :time
     add_column :default_assignments, :slot_number, :integer
     DefaultAssignment.find(:all).each{|x|
-      i = 1
-      x.volunteer_default_shift.slot_count.times do
-        x.start_time = x.volunteer_default_shift.start_time
-        x.end_time = x.volunteer_default_shift.end_time
-        x.slot_number = i
-        x.save!
+      x.start_time = x.volunteer_default_shift.start_time
+      x.end_time = x.volunteer_default_shift.end_time
+      x.slot_number = 1
+      x.save!
+      i = 2
+      (x.volunteer_default_shift.slot_count - 1).times do
+        y = DefaultAssignment.new
+        y.volunteer_default_shift_id = x.volunteer_default_shift_id
+        y.start_time = x.volunteer_default_shift.start_time
+        y.end_time = x.volunteer_default_shift.end_time
+        y.slot_number = i
+        y.save!
         i += 1
       end
     }
