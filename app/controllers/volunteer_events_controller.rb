2730692a8bdeb550658c42282504d46993bbc147
Ryan Niebur <ryanryan52@gmail.com>
2011-05-20 15:58:23 -0700

    add new class page for volunteer scheduling

--- app/controllers/volunteer_events_controller.rb
+++ app/controllers/volunteer_events_controller.rb
@@ -16,6 +16,86 @@ class VolunteerEventsController < ApplicationController
     @volunteer_event = VolunteerEvent.find(params[:id])
   end
 
+  def new_class
+    @notice = ""
+    c = params[:klass]
+    if c.nil?
+      render :action => "new_class"
+      return
+    end
+    puts c.inspect
+    normal = c[:student_slot_count].to_i || 0
+    instructor = c[:instructor_slot_count].to_i || 0
+    audit = c[:audit_slot_count].to_i || 0
+    roster = Roster.find_by_id(c[:roster_id].to_i)
+    program = Program.find_by_id(c[:program_id].to_i)
+#    if roster.nil? or program.nil?
+#      redirect_to :action => "new_class"
+#      return
+#    end
+    start_hour = c["start_time(4i)"].to_i
+    start_minute = c["start_time(5i)"].to_i
+    start_hour += 12 if c["start_time(7i)"].to_i == ActionView::Helpers::DateTimeSelector::PM
+    start_time = Time.parse([start_hour, start_minute].join(":"))
+    end_hour = c["end_time(4i)"].to_i
+    end_minute = c["end_time(5i)"].to_i
+    end_hour += 12 if c["end_time(7i)"].to_i == ActionView::Helpers::DateTimeSelector::PM
+    end_time = Time.parse([end_hour, end_minute].join(":"))
+    c["start_time"] = start_time
+    c["end_time"] = end_time
+    c["program"] = program
+    c["roster"] = roster
+    @volunteer_event = VolunteerEvent.new
+    @volunteer_event.description = c[:description]
+    @volunteer_event.date = c[:date]
+    @volunteer_event.notes = c[:notes]
+    normal.times do |i|
+      a = VolunteerShift.new
+      a.class_credit = true
+      a.roster_id = roster.id if roster
+      a.program_id = program.id if program
+      a.slot_number = (i + 1) # starts 0
+      a.start_time = start_time
+      a.end_time = end_time
+      @volunteer_event.volunteer_shifts << a
+    end
+    audit.times do |i|
+      a = VolunteerShift.new
+      a.class_credit = false
+      a.description = "AUDIT"
+      a.roster_id = roster.id if roster
+      a.program_id = program.id if program
+      a.slot_number = (normal + i + 1) # starts 0
+      a.start_time = start_time
+      a.end_time = end_time
+      @volunteer_event.volunteer_shifts << a
+    end
+    instructor.times do |i|
+      a = VolunteerShift.new
+      a.class_credit = false
+      a.volunteer_task_type = VolunteerTaskType.find_by_name("teaching")
+      a.slot_number = (i + 1)
+      a.roster_id = roster.id if roster
+      a.program_id = program.id if program
+      a.start_time = start_time
+      a.end_time = end_time
+      @volunteer_event.volunteer_shifts << a
+    end
+    puts @volunteer_event.inspect
+    puts @volunteer_event.volunteer_shifts.inspect
+    if @volunteer_event.save
+      @volunteer_event = VolunteerEvent.new
+      @notice = "\"#{c[:description]}\" created successfully"
+      render :action => "new_class"
+    else
+      def c.method_missing(sym)
+        self[sym]
+      end
+      @klass = c
+      render :action => "new_class"
+    end
+  end
+
   def new
     @volunteer_event = VolunteerEvent.new
   end
