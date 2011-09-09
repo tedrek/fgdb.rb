653445b27b779d41b01954cb0cbc947b70c05532
Ryan Niebur <ryanryan52@gmail.com>
2011-09-08 22:58:54 -0700

    generate even if we are closed that day...if the shifts there, it was put there for a reason

--- app/models/volunteer_default_shift.rb
+++ app/models/volunteer_default_shift.rb
@@ -157,7 +157,7 @@ class VolunteerDefaultShift < ActiveRecord::Base
     (start_date..end_date).each{|x|
       next if Holiday.is_holiday?(x)
       w = Weekday.find(x.wday)
-      next if !w.is_open
+#      next if !w.is_open
       vs_conds = gconditions.dup
       vs_conds.date_enabled = "true"
       vs_conds.date_date_type = 'daily'
