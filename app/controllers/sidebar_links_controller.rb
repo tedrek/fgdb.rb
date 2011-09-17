c734c8889df494f2907f7263f6510259317eb03e
Ryan Niebur <ryanryan52@gmail.com>
2011-09-17 13:58:23 -0700

    add a recent crash page (Closes: #24513)

--- app/controllers/sidebar_links_controller.rb
+++ app/controllers/sidebar_links_controller.rb
@@ -4,12 +4,23 @@ class SidebarLinksController < ApplicationController
   protected
   def get_required_privileges
     a = super
-    a << {:privileges => ['role_admin'], :only => ["crash"]}
+    a << {:privileges => ['role_admin'], :only => ["crash", "recent_crash"]}
     a
   end
 
 
   public
+  def recent_crash
+    d = File.join(RAILS_ROOT, "tmp", "crash")
+    @first = `ls -t #{d} | head -30`.split("\n").map{|x| x.split(".").last}
+    @strs = {}
+    for file in @first
+      f = File.join(RAILS_ROOT, "tmp", "crash", "crash." + file)
+      data = JSON.parse(File.read(f))
+      @strs[file] = data
+    end
+  end
+
   def crash
     f = File.join(RAILS_ROOT, "tmp", "crash", "crash." + params[:id])
     if !File.exist?(f)
