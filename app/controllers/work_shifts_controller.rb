e531ace6d4d4c8ae5558d93ad4c871c54609df73
Ryan Niebur <ryanryan52@gmail.com>
2011-10-29 17:08:38 -0700

    show worker conflicts so the report finds problems of people scheduled at the same time

--- app/controllers/work_shifts_controller.rb
+++ app/controllers/work_shifts_controller.rb
@@ -11,6 +11,7 @@ class WorkShiftsController < ApplicationController
   def find_problems
     @start_date = Date.parse(params[:start_date])
     @end_date = Date.parse(params[:end_date])
+    @conflicts = DB.exec(DB.prepare_sql("SELECT w1.shift_date AS date,workers.name AS worker,COALESCE(job1.name,w1.meeting_name,w1.kind) AS job_1,COALESCE(job2.name,w2.meeting_name,w2.kind) AS job_2,w1.id AS shift_1,w2.id AS shift_2 FROM work_shifts AS w1 INNER JOIN work_shifts AS w2 ON w1.worker_id = w2.worker_id AND w1.shift_date = w2.shift_date AND ((w1.start_time < w2.end_time AND w2.start_time < w1.end_time) OR (w1.start_time > w2.end_time AND w2.start_time > w1.end_time)) AND w1.id < w2.id AND w1.worker_id != 0 LEFT JOIN jobs AS job1 ON job1.id = w1.job_id LEFT JOIN jobs AS job2 ON job2.id = w2.job_id LEFT JOIN workers ON w1.worker_id = workers.id WHERE w1.shift_date >= ? AND w1.shift_date <= ? ORDER BY 1,2;", @start_date, @end_date)).to_a.map{|x| "On #{x["date"]}, #{x["worker"]} is scheduled in #{x["job_1"]} and #{x["job_2"]} at the same time" }
     @all_dates = (@start_date..@end_date).to_a.select{|x| Weekday.find_by_id(x.wday).is_open}
     @jobs = Job.find_all_by_coverage_type_id(CoverageType.find_by_name("full").id)
     all_shifts = WorkShift.find(:all, :conditions => ['job_id IN (?) AND shift_date >= ? AND shift_date <= ?', @jobs.map{|x| x.id}, @start_date, @end_date])
