d60116541e08364bbf4781be941f7199d8bfe85a
Ryan Niebur <ryanryan52@gmail.com>
2011-12-17 02:12:31 -0800

    if the privilege type is restrict and the user is shared, do not give them it

--- app/models/user.rb
+++ app/models/user.rb
@@ -134,6 +134,7 @@ class User < ActiveRecord::Base
     olda = []
     a = [self, self.contact, self.contact ? self.contact.worker : nil, self.roles].flatten.select{|x| !x.nil?}.map{|x| x.to_privileges}.flatten.select{|x| !x.nil?}.map{|x| Privilege.by_name(x)}
     while olda != a
+      a = a.select{|x| !x.restrict} if self.shared
       olda = a.dup
       a << olda.map{|x| x.children}.flatten
       a = a.flatten.sort_by(&:name).uniq
