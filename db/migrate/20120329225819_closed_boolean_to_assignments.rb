018362eaa152ea7579d3e284eaf0004db611d31a
Ryan Niebur <ryanryan52@gmail.com>
2012-03-29 17:08:35 -0700

    complete migration to transfer from and remove the no work contacts

--- db/migrate/20120329225819_closed_boolean_to_assignments.rb
+++ db/migrate/20120329225819_closed_boolean_to_assignments.rb
@@ -1,8 +1,20 @@
 class ClosedBooleanToAssignments < ActiveRecord::Migration
   def self.up
     add_column :assignments, :closed, :boolean, :default => false, :null => false
+    a = [91069, 92616]
+    a.each do |d|
+      c = Contact.find_by_id(d)
+      if ! c
+        puts "WARNING: Couldn't find no work contact ##{d}"
+        next
+      end
+      DB.exec("UPDATE assignments SET contact_id = NULL AND closed = 't' WHERE contact_id = #{c.id};")
+      puts "Removing fake contact #{c.display_name}.."
+      c.destroy!
+    end
   end
 
   def self.down
+    remove_column :assignments, :closed
   end
 end
