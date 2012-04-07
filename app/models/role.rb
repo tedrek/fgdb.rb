8d6de85c79d5a08b03076e8f7eabd498b39b9bcb
Ryan Niebur <ryanryan52@gmail.com>
2012-04-07 14:56:26 -0700

    reformat roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,8 +3,8 @@ class Role < ActiveRecord::Base
   has_and_belongs_to_many :privileges
   has_and_belongs_to_many "users"
 
-  def display_string
-    self.name + " (" + self.privileges.map{|x| x.name + (x.restrict ? '*' : '')}.join(", ") + ")"
+  def privilege_list
+    self.privileges.map{|x| x.name + (x.restrict ? '*' : '')}.join(", ")
   end
 
   def to_privileges
