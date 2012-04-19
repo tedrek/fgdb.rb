35092e23de7d9c9fa9e8295b93f9d0ad18eb32ff
Ryan Niebur <ryanryan52@gmail.com>
2012-04-19 14:54:40 -0700

    Remove habtm association between jobs and workers

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,6 +1,5 @@
 class Worker < ActiveRecord::Base
   has_many :standard_shifts
-  has_and_belongs_to_many :jobs
   belongs_to :worker_type
   has_and_belongs_to_many :meetings
   has_many :work_shifts
