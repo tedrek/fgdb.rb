b1ff954b3953bbd3815e0609b34d1e3f401ce466
Ryan Niebur <ryanryan52@gmail.com>
2012-04-21 15:29:19 -0700

    fix link for individual worker report

--- app/helpers/worked_shifts_helper.rb
+++ app/helpers/worked_shifts_helper.rb
@@ -3,7 +3,7 @@ module WorkedShiftsHelper
     worker = worker.id if worker.class == Worker
     date = date.to_date if date.class == DateTime
     date = date.to_s if date.class == Date
-    h = {:controller => "worked_shifts", :action => "index", :worked_shift => {:worker_id => worker, :date_performed => date}}
+    h = {:controller => "worked_shifts", :action => "index", :worked_shift => {:contact_id => worker.contact_id, :date_performed => date}}
     if controller
       return controller.url_for h
     else
