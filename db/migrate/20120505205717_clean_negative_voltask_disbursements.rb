55e5e9b2b0cd58b244c4d6b6ab6866e58cbbd787
Ryan Niebur <ryanryan52@gmail.com>
2012-05-05 14:14:46 -0700

    Clean up negative volunteer tasks, replacing them with disbursement records when appropriate (Closes: #20108)

---
+++ db/migrate/20120505205717_clean_negative_voltask_disbursements.rb
@@ -0,0 +1,32 @@
+class CleanNegativeVoltaskDisbursements < ActiveRecord::Migration
+  def self.up
+    ActiveRecord::Base.record_timestamps = false
+
+    VolunteerTask.find(:all, :conditions => ['duration <= 0']).each do |vt|
+      dt = nil
+      if [0, -12, -24, -36, -48, -60].include(vt.duration)
+        if vt.duration == 0
+          if vt.contact and vt.contact.is_organization
+            dt = DisbursementType.find_by_name("hardware_grants")
+          end
+        else
+          dt = DisbursementType.find_by_name("adoption")
+        end
+      end
+
+      if dt
+        d = Disbursement.new
+        d.comments "Disbursement automatically created from volunteer task with #{vt.duration} hours"
+        d.contact_id = vt.contact_id
+        d.disbursement_type_id = dt.id
+        d.disbursed_at = vt.date_performed
+        d.save!
+      end
+
+      vt.destroy!
+    end
+  end
+
+  def self.down
+  end
+end
