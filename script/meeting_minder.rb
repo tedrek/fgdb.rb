9a0c377be2e1152439601c4aee0c82017d91bacf
Ryan Niebur <ryanryan52@gmail.com>
2012-05-13 12:36:14 -0700

    idea

--- script/meeting_minder.rb
+++ script/meeting_minder.rb
@@ -5,4 +5,6 @@ ENV['RAILS_ENV']="production"
 require File.dirname(__FILE__) + '/../config/boot'
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 
+# TODO: 7am at first, but then can have --morning and --afternoon for 3pm too (and evening, if needed), with a new column on the minders
+
 MeetingMinder.send_all
