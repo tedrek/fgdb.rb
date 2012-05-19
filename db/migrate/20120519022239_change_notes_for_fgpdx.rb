02a8b6521aa41539dfe57c7541ae66d4b291ef3d
Ryan Niebur <ryanryan52@gmail.com>
2012-05-19 10:25:01 -0700

    line break in the notes

--- db/migrate/20120519022239_change_notes_for_fgpdx.rb
+++ db/migrate/20120519022239_change_notes_for_fgpdx.rb
@@ -1,7 +1,7 @@
 class ChangeNotesForFgpdx < ActiveRecord::Migration
   def self.up
     if Default.is_pdx and (contract = Contract.find_by_name("city"))
-      contract.notes = "FG-PDXs are earmarked for Portland residents. Make sure the contact is a Portland resident before completing this disbursement."
+      contract.notes = "FG-PDXs are earmarked for Portland residents.\nMake sure the contact is a Portland resident before completing this disbursement."
       contract.save!
     else
       puts "Skipping change to city contract for non-pdx database."
