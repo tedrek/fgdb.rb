6cb1069adb122d8967d48e93d350f06294aa88af
Ryan Niebur <ryanryan52@gmail.com>
2012-05-18 19:26:19 -0700

    update the displayed notes when disbursing fg-pdx systems (Closes: #28466)

---
+++ db/migrate/20120519022239_change_notes_for_fgpdx.rb
@@ -0,0 +1,13 @@
+class ChangeNotesForFgpdx < ActiveRecord::Migration
+  def self.up
+    if Default.is_pdx and (contract = Contract.find_by_name("city"))
+      contract.notes = "FG-PDXs are earmarked for Portland residents. Make sure the contact is a Portland resident before completing this disbursement."
+      contract.save!
+    else
+      puts "Skipping change to city contract for non-pdx database."
+    end
+  end
+
+  def self.down
+  end
+end
