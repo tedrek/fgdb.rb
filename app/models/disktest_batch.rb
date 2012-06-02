da61d85882395c8ccdd2904f62b4f6a997980f56
Ryan Niebur <ryanryan52@gmail.com>
2012-06-02 13:23:23 -0700

    fix association and add the drive count to the output list

--- app/models/disktest_batch.rb
+++ app/models/disktest_batch.rb
@@ -1,6 +1,6 @@
 class DisktestBatch < ActiveRecord::Base
   belongs_to :contact
-  has_many :disktest_batch_drives
+  has_many :disktest_batch_drives, :class_name => "DisktestBatchDrive"
 
   validates_presence_of :name
   validates_presence_of :date
