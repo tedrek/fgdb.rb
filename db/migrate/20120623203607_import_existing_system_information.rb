f7582cf9c4cc5ad674a508daef3b049592af415c
Ryan Niebur <ryanryan52@gmail.com>
2012-06-29 13:59:10 -0700

    fix migration to happen in batches, in hopes of correcting the memory issue

--- db/migrate/20120623203607_import_existing_system_information.rb
+++ db/migrate/20120623203607_import_existing_system_information.rb
@@ -1,17 +1,11 @@
 class ImportExistingSystemInformation < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.record_timestamps = false
-    count = 0
-    sheets = SpecSheet.find(:all, :conditions => ["id IN (SELECT DISTINCT ON(system_id) id FROM spec_sheets ORDER BY system_id, created_at DESC)"])
-    inc = (sheets.length / 100.0).to_i
-    puts "Importing existing data:"
-    sheets.each do |sheet|
-      count += 1
-      if (count % inc) == 0
-        puts "#{100 * count / sheets.length}% complete"
+    SpecSheet.find_in_batches(:conditions => ["id IN (SELECT DISTINCT ON(system_id) id FROM spec_sheets ORDER BY system_id, created_at DESC)"]) do |sheets|
+      sheets.each do |sheet|
+        sheet.set_extra_system_information(sheet.parser)
+        sheet.save
       end
-      sheet.set_extra_system_information(sheet.parser)
-      sheet.save
     end
     ActiveRecord::Base.record_timestamps = true
   end
