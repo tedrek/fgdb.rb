38f7e24f3009d823d506740d1262488e10ae27cd
Ryan Niebur <ryanryan52@gmail.com>
2012-06-29 13:34:51 -0700

    set default values for jobs

--- db/migrate/20120627235125_add_offsite_bool_to_staff_scheduling_and_hours.rb
+++ db/migrate/20120627235125_add_offsite_bool_to_staff_scheduling_and_hours.rb
@@ -4,6 +4,13 @@ class AddOffsiteBoolToStaffSchedulingAndHours < ActiveRecord::Migration
     add_column :work_shifts, :offsite, :boolean, :null => false, :default => false
     add_column :shifts, :offsite, :boolean, :null => false, :default => false
     add_column :worked_shifts, :offsite, :boolean
+
+    Job.each do |x|
+      if x.name.match(/off-?site/i)
+        x.offsite = true
+        x.save
+      end
+    end
   end
 
   def self.down
