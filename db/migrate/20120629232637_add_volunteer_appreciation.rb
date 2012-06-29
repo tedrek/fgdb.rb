0b64b37a0bc0a01ce97f1610c28b117db2bdc69d
Ryan Niebur <ryanryan52@gmail.com>
2012-06-29 16:45:17 -0700

    Add volunteer appreciation disbursement and gizmo types (Closes: #29178)

---
+++ db/migrate/20120629232637_add_volunteer_appreciation.rb
@@ -0,0 +1,33 @@
+class AddVolunteerAppreciation < ActiveRecord::Migration
+  def self.up
+    if Default.is_pdx
+      d = DisbursementType.new
+      d.name = "volunteer_appreciation_gifts"
+      d.description = "Vol Appreciation Gifts"
+      d.save!
+
+      for i in [300, 600, 1000, 2000, 3000]
+        g = GizmoType.new
+        g.name = "#{i}_volunteer_appreciation_gift"
+        if i == 3000
+          g.description = "Vol Appreciation Gift, #{i}+ hours"
+        else
+          g.description = "Vol Appreciation Gift, #{i} hours"
+        end
+        g.required_fee_cents = 0
+        g.suggested_fee_cents = 0
+        g.gizmo_category_id = GizmoCategory.find_by_name('misc')
+        g.covered = false
+        g.rank = 99
+        g.parent_name = "gizmo"
+        g.needs_id = false
+        g.return_policy_id = nil
+        g.gizmo_contexts << GizmoContext.disbursement
+        g.save!
+      end
+    end
+  end
+
+  def self.down
+  end
+end
