795120cfc1952bbff83a2dae510594011235612d
Ryan Niebur <ryanryan52@gmail.com>
2012-08-01 16:19:44 -0700

    Pull the type out as well

--- app/models/spec_sheet.rb
+++ app/models/spec_sheet.rb
@@ -17,7 +17,11 @@ class SpecSheet < ActiveRecord::Base
   has_many :spec_sheet_values, :include => [:spec_sheet_question], :order => "spec_sheet_questions.position ASC"
 
   def pricing_values
-    self.parser.pricing_values
+    h = self.parser.pricing_values
+    h[:build_type] = self.type ? self.type.name : nil
+    a = h.instance_variable_get("@arr")
+    a.unshift(a.pop)
+    h
   end
 
   # is there already an inverse to Hash.to_a ?
