5aa1dc14f3182c836410931bbf2f5c8bb4232f48
Ryan Niebur <ryanryan52@gmail.com>
2012-08-06 00:04:05 -0700

    fix the HABTM table to not have an id

--- db/migrate/20120803213659_create_pricing_values_system_pricings.rb
+++ db/migrate/20120803213659_create_pricing_values_system_pricings.rb
@@ -1,10 +1,8 @@
 class CreatePricingValuesSystemPricings < ActiveRecord::Migration
   def self.up
-    create_table :pricing_values_system_pricings do |t|
+    create_table :pricing_values_system_pricings, :id => false do |t|
       t.integer :pricing_value_id
       t.integer :system_pricing_id
-
-      t.timestamps
     end
   end
 
