373c62211ebfb32f50da4d5dd72f0051eb5ad9c8
Ryan Niebur <ryanryan52@gmail.com>
2012-08-15 12:24:57 -0700

    Fix vol appreciation gizmo types to be in misc gizmo category

---
+++ db/migrate/20120815192336_fix_category_on_vol_appreciation_gizmo_types.rb
@@ -0,0 +1,12 @@
+class FixCategoryOnVolAppreciationGizmoTypes < ActiveRecord::Migration
+  def self.up
+    for i in [300, 600, 1000, 2000, 3000]
+      gt = GizmoType.find_by_name("#{i}_volunteer_appreciation_gift")
+      gt.gizmo_category_id = GizmoCategory.find_by_name('misc').id
+      gt.save!
+    end
+  end
+
+  def self.down
+  end
+end
