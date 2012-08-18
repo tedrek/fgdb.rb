75e74ab3500f9c50d99a2bee745e621e13082066
Ryan Niebur <ryanryan52@gmail.com>
2012-08-18 14:24:17 -0700

    Remove word Donation from receipt when it should not show up, display the suggested fee text by default

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -351,9 +351,10 @@ class TransactionController < ApplicationController
     pdf.stroke_horizontal_rule
 
     w = 540/3.0
-    pdf.table(    [[{:content => "#{(@txn.invoiced? && receipt_type == 'invoice') ? "Invoice" : "Donation Receipt"}"}, {:content => ""}, {:content => Default["tax id"], :align => :right}],
+    show_context = @txn.class != Donation || (receipt_type == 'receipt' && (@txn.find_lines(:is_gizmo_line?).length > 0 || @txn.cash_donation_paid_cents > 0))
+    pdf.table(    [[{:content => "#{(@txn.invoiced? && receipt_type == 'invoice') ? "Invoice" : show_context ? "Donation Receipt" : "Invoice Receipt"}"}, {:content => ""}, {:content => Default["tax id"], :align => :right}],
      [{:content => "Created by ##{User.find_by_id(@transaction.cashier_created_by).contact_id}"}],
-     [{:content => "Date: #{@txn.occurred_at.strftime("%m/%d/%Y")}"}, {:content => "Donation ##{@txn.id}"}, {:content => @txn.invoiced? ? "Due: #{@transaction.created_at.+(60*60*24*30).strftime("%m/%d/%Y")}" : "", :align => :right}]], :column_widths => [w, w, w], :cell_style => {:border_width => 0, :padding => 0})
+                   [{:content => "Date: #{@txn.occurred_at.strftime("%m/%d/%Y")}"}, {:content => "#{show_context ? "Donation" : "Invoice"} ##{@txn.id}"}, {:content => @txn.invoiced? ? "Due: #{@transaction.created_at.+(60*60*24*30).strftime("%m/%d/%Y")}" : "", :align => :right}]], :column_widths => [w, w, w], :cell_style => {:border_width => 0, :padding => 0})
 
     if show_suggested
       pdf.text "There is a suggested tax deductible contribution of $#{@txn.reported_suggested_fee} for these items.", :font_size => font_size
