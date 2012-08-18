035aa2ad380c5b58926d1b86cef8c0d15172f9ad
Ryan Niebur <ryanryan52@gmail.com>
2012-08-17 17:40:27 -0700

    Allow the STORE_ADMIN role to see created and updated by conditions (Closes: #30212)

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,6 +2,12 @@ class SalesController < TransactionController
   protected
 
   private
+  def get_required_privileges
+    a = super
+    a << {:only => ["/show_created_and_updated_by"], :privileges => ['role_store_admin']}
+    return a
+  end
+
   def new_trans_init_hook
     @transaction.discount_schedule_id = DiscountSchedule.find_by_name('no_discount').id
   end
