4bdad49e8de223dfeceb70b8f100d068bb384436
Ryan Niebur <ryanryan52@gmail.com>
2012-09-05 11:43:35 -0700

    Fix conditions that the report looks at to contain schedule conditions when needed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -70,11 +70,18 @@ class ApplicationController < ActionController::Base
 
   def do_find_problems_report(klass, column, weeks, dates = [], mode = "ws", schedule_id = nil)
     @start_date, @end_date = dates
-    @conflicts = DB.exec(DB.prepare_sql("SELECT w1.#{column} AS date,workers.name AS worker,COALESCE(job1.name,w1.meeting_name,w1.#{mode == "ws" ? "kind" : "type"}) AS job_1,COALESCE(job2.name,w2.meeting_name,w2.#{mode == "ws" ? "kind" : "type"}) AS job_2,w1.id AS shift_1,w2.id AS shift_2 FROM #{klass.table_name} AS w1 INNER JOIN #{klass.table_name} AS w2 ON w1.worker_id = w2.worker_id AND w1.#{column} = w2.#{column} AND ((w1.start_time < w2.end_time AND w2.start_time < w1.end_time) OR (w1.start_time > w2.end_time AND w2.start_time > w1.end_time)) AND w1.id < w2.id AND w1.worker_id != 0 #{mode == "ws" ? "" : " AND w1.shift_date IS NULL AND w2.shift_date IS NULL AND (w1.ineffective_date IS NULL OR w1.ineffective_date >= '" + Date.today.to_s + "') AND (w2.ineffective_date IS NULL OR w2.ineffective_date >= '" + Date.today.to_s + "')"} LEFT JOIN jobs AS job1 ON job1.id = w1.job_id LEFT JOIN jobs AS job2 ON job2.id = w2.job_id LEFT JOIN workers ON w1.worker_id = workers.id WHERE w1.#{column} >= ? AND w1.#{column} <= ? ORDER BY 1,2;", @start_date, @end_date)).to_a
+    opts = [@start_date, @end_date]
+    if schedule_id
+      opts << schedule_id
+      opts << schedule_id
+    end
+    @conflicts = DB.exec(DB.prepare_sql("SELECT w1.#{column} AS date,workers.name AS worker,COALESCE(job1.name,w1.meeting_name,w1.#{mode == "ws" ? "kind" : "type"}) AS job_1,COALESCE(job2.name,w2.meeting_name,w2.#{mode == "ws" ? "kind" : "type"}) AS job_2,w1.id AS shift_1,w2.id AS shift_2 FROM #{klass.table_name} AS w1 INNER JOIN #{klass.table_name} AS w2 ON w1.worker_id = w2.worker_id AND w1.#{column} = w2.#{column} AND ((w1.start_time < w2.end_time AND w2.start_time < w1.end_time) OR (w1.start_time > w2.end_time AND w2.start_time > w1.end_time)) AND w1.id < w2.id AND w1.worker_id != 0 #{mode == "ws" ? "" : " AND w1.shift_date IS NULL AND w2.shift_date IS NULL AND (w1.ineffective_date IS NULL OR w1.ineffective_date >= '" + Date.today.to_s + "') AND (w2.ineffective_date IS NULL OR w2.ineffective_date >= '" + Date.today.to_s + "')"} LEFT JOIN jobs AS job1 ON job1.id = w1.job_id LEFT JOIN jobs AS job2 ON job2.id = w2.job_id LEFT JOIN workers ON w1.worker_id = workers.id WHERE w1.#{column} >= ? AND w1.#{column} <= ? #{schedule_id ? " AND w1.schedule_id = ? AND w2.schedule_id = ?" : ""} ORDER BY 1,2;", *opts)).to_a
     @unassigned = DB.exec(DB.prepare_sql("SELECT w1.#{column} AS date,COALESCE(job1.name,w1.meeting_name,w1.#{mode == "ws" ? "kind" : "type"}) || case training when 't' then ' (Training)' else '' end AS job FROM #{klass.table_name} AS w1 LEFT JOIN jobs AS job1 ON job1.id = w1.job_id WHERE #{klass == Shift ? "type NOT LIKE 'Meeting' AND" : ""} w1.worker_id = 0 #{mode == "ws" ? "" : " AND w1.shift_date IS NULL  AND (w1.ineffective_date IS NULL OR w1.ineffective_date >= '" + Date.today.to_s + "')"} AND w1.#{column} >= ? AND w1.#{column} <= ? ORDER BY 1,2;", @start_date, @end_date)).to_a
     @all_dates = (dates.first..dates.last).to_a.select{|x| Weekday.find_by_id(mode == "ws" ? x.wday : x).is_open}
     @jobs = Job.find_all_by_coverage_type_id(CoverageType.find_by_name("full").id)
-    all_shifts = klass.find(:all, :conditions => ["#{mode == "ws" ? "" : " shift_date IS NULL AND (ineffective_date IS NULL OR ineffective_date >= '" + Date.today.to_s + "') AND "} (training = 'f' OR training IS NULL) AND job_id IN (?) AND #{column} >= ? AND #{column} <= ?", @jobs.map{|x| x.id}, @start_date, @end_date])
+    copts = [@jobs.map{|x| x.id}, @start_date, @end_date]
+    copts << schedule_id if schedule_id
+    all_shifts = klass.find(:all, :conditions => ["#{mode == "ws" ? "" : " shift_date IS NULL AND (ineffective_date IS NULL OR ineffective_date >= '" + Date.today.to_s + "') AND "} (training = 'f' OR training IS NULL) AND job_id IN (?) AND #{column} >= ? AND #{column} <= ? #{schedule_id ? ' AND schedule_id = ?' : ''}", *copts])
     @shift_gap_hash = {}
     weekday_times = {}
     Weekday.find(:all).each do |w|
@@ -94,7 +101,11 @@ class ApplicationController < ActionController::Base
     @workers_day_hash = {}
     @workers = []
     @workers_h = {}
-    all_scheduled = klass.find(:all, :conditions => ["#{mode == "ws" ? "" : " shift_date IS NULL AND (ineffective_date IS NU'LL OR ineffective_date >= '" + Date.today.to_s + "') AND "} #{column} >= ? AND #{column} <= ? AND #{mode == "ws" ? "kind" : "type"} NOT LIKE 'Unavailability'", @weeks.first, (@weeks.last+6)], :order => "#{column} ASC")
+    dopts = [@weeks.first, (@weeks.last+6)]
+    if schedule_id
+      dopts << schedule_id
+    end
+    all_scheduled = klass.find(:all, :conditions => ["#{mode == "ws" ? "" : " shift_date IS NULL AND (ineffective_date IS NULL OR ineffective_date >= '" + Date.today.to_s + "') AND "} #{column} >= ? AND #{column} <= ? AND #{mode == "ws" ? "kind" : "type"} NOT LIKE 'Unavailability' #{schedule_id ? ' AND schedule_id = ?': ''}", *dopts], :order => "#{column} ASC")
     cur_week = @weeks.first
     all_scheduled.each{|w|
       if !@workers.include?(w.worker)
