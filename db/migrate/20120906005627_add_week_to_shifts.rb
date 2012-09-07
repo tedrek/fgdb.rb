a8226566e2392779b1be46081fca514ae83425d1
Ryan Niebur <ryanryan52@gmail.com>
2012-09-07 14:27:06 -0700

    remove the unused A/B-week schedules from the main schedule during migration (Closes: #30612)

--- db/migrate/20120906005627_add_week_to_shifts.rb
+++ db/migrate/20120906005627_add_week_to_shifts.rb
@@ -1,6 +1,16 @@
 class AddWeekToShifts < ActiveRecord::Migration
   def self.up
     add_column :shifts, :week, :character
+    main = Schedule.find_by_name("main")
+    if main && Default.is_pdx
+      desc = Schedule.find_all_by_parent_id(main.id)
+      from = desc.map(&:id)
+      to = main.id
+      for i in [:shifts, :standard_shifts, :holidays, :work_shifts]
+        DB.exec(['UPDATE ' + i.to_s + ' SET schedule_id = ? WHERE schedule_id IN (?)', to, from])
+      end
+      desc.each(&:destroy)
+    end
   end
 
   def self.down
