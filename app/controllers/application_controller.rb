52bbf264146f6796023a4d42f7194a850edffa0e
Ryan Niebur <ryanryan52@gmail.com>
2012-09-21 15:48:50 -0700

    Fix work shifts footnotes, which somehow broke since this morning?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,6 +46,7 @@ class ApplicationController < ActionController::Base
       @footnote = ShiftFootnote.find_or_create_by_weekday_id_and_schedule_id(@date, @schedule)
       @vacs = []
     else
+      @date = @date.to_date
       @vacs = Vacation.on_date(@date)
       @footnote = WorkShiftFootnote.find_or_create_by_date(@date)
     end
