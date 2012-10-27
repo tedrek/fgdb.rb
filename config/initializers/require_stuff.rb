7556a4e4eb394d060652747c5bee635627d06fce
Ryan Niebur <ryanryan52@gmail.com>
2012-10-26 19:33:09 -0700

    Wrap around will_paginate to make all search results automatically include a string such as 'Results 1 - 20 of 55764' (Closes: #31637)

--- config/initializers/require_stuff.rb
+++ config/initializers/require_stuff.rb
@@ -19,3 +19,24 @@ module ActionView
     end
   end
 end
+
+module WillPaginate
+  module ViewHelpers
+    def will_paginate_with_total_results(will = nil, *opts)
+      ret = will_paginate_without_total_results(will, *opts)
+      if will
+        start = (will.current_page-1)*will.per_page + 1
+        finish = start + will.size - 1
+        if finish == 0
+          return ret + "No results found"
+        elsif start != finish
+          return ret + "Results " + start.to_s + " - " + finish.to_s + " of " + will.total_entries.to_s
+        else
+          return ret + "Result " + finish.to_s + " of " + will.total_entries.to_s
+        end
+      end
+      return ret
+    end
+    alias_method_chain :will_paginate, :total_results
+  end
+end
