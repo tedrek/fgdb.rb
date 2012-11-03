7bfddd2ca86439a13d61522db4bd8aa62ce1ba35
Ryan Niebur <ryanryan52@gmail.com>
2012-11-03 15:44:42 -0700

    Save the multiplier as a cents value

--- db/migrate/20121103185258_add_multiplier_to_components_and_expression.rb
+++ db/migrate/20121103185258_add_multiplier_to_components_and_expression.rb
@@ -1,11 +1,11 @@
 class AddMultiplierToComponentsAndExpression < ActiveRecord::Migration
   def self.up
-    add_column :pricing_components, :multiplier, :float, :default => 1.0, :null => false
-    add_column :pricing_expressions, :multiplier, :float, :default => 1.0, :null => false
+    add_column :pricing_components, :multiplier_cents, :integer, :default => 100, :null => false
+    add_column :pricing_expressions, :multiplier_cents, :integer, :default => 100, :null => false
   end
 
   def self.down
-    remove_column :pricing_components, :multiplier
-    remove_column :pricing_expressions, :multiplier
+    remove_column :pricing_components, :multiplier_cents
+    remove_column :pricing_expressions, :multiplier_cents
   end
 end
