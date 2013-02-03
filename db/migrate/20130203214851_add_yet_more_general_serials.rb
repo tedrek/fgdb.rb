2795c73dcc0fac744c98abb8a5f57e4ad1bd5971
root <root@zhora.fglan>
2013-02-03 14:25:17 -0800

    Improve the method used to detect generics to know about current system id's and add a bunch that I found now (Closes: #33525)

--- db/migrate/20130203214851_add_yet_more_general_serials.rb
+++ db/migrate/20130203214851_add_yet_more_general_serials.rb
@@ -1,8 +1,7 @@
 class AddYetMoreGeneralSerials < ActiveRecord::Migration
   def self.up
-    for i in ["= '00000000'", "EeePC-1234567890", "01234567", "..              ."]
-      g = Generic.new
-      g.value = i
+    for i in ["= '00000000'", "EeePC-1234567890", "01234567", "..              .", "Unknow", "0123456789AB", "SystemSerialNumb", "12", "12 4", "SSN12345678901234567", "Not Available", "Bank0/1", "FIELD", "X312345678", "Sat May 05 22:08:37 2007"]
+      g = Generic.find_or_create_by_value(i)
       g.only_serial = true
       g.usable = true
       g.save!
--- script/find-generics.sql
+++ script/find-generics.sql
@@ -1,4 +1,4 @@
 #!/bin/sh
 exec ruby `dirname $0`/sql_runner $0 "$@"
 
-SELECT system_id AS id, systems.vendor, systems.model, '***' || systems.serial_number || '***' AS serial_number, max(spec_sheets.created_at) AS last_created, count(*) AS count FROM spec_sheets JOIN systems ON systems.id = system_id WHERE length(systems.serial_number) > 5 AND systems.serial_number NOT IN (SELECT value FROM generics) AND systems.vendor NOT IN (SELECT value FROM generics WHERE only_serial = 'f') AND systems.model NOT IN (SELECT value FROM generics WHERE only_serial = 'f') GROUP BY system_id, systems.vendor, systems.model, systems.serial_number HAVING count(*) > 5 ORDER by count DESC;
+SELECT '"' || systems.serial_number || '"',count(DISTINCT system_id) AS system_count,count(*) AS spec_sheet_count, max(spec_sheets.created_at) AS last_created FROM spec_sheets LEFT OUTER JOIN systems ON spec_sheets.system_id = systems.id WHERE systems.serial_number NOT LIKE '(no serial number)' AND systems.serial_number NOT IN (SELECT value FROM generics WHERE usable = 't') GROUP BY 1 HAVING count(*) > 5 ORDER BY system_count desc, spec_sheet_count desc;
