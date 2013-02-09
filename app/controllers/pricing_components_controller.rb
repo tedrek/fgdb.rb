2dfd52970c4fa8e3785896c39ea5cf53566a29f5
Ryan Niebur <ryanryan52@gmail.com>
2013-02-09 12:26:44 -0800

    Implement loading from CSV for pricing values and also fix up some other things

--- app/controllers/pricing_components_controller.rb
+++ app/controllers/pricing_components_controller.rb
@@ -8,7 +8,7 @@ class PricingComponentsController < ApplicationController
     return a
   end
 
-  def make_a_table
+  def prep_table_headers
     @pricing_values = @pricing_component.pricing_values
     header = ['ID', 'Name']
     if @pricing_component and @pricing_component.pull_from
@@ -19,9 +19,12 @@ class PricingComponentsController < ApplicationController
       header << 'Matcher'
       end
       end
-    header << 'Value cents'
+    header << 'Value'
     @table = [header]
+  end
 
+  def make_a_table
+    prep_table_headers
     for pricing_value in @pricing_values
       p = [pricing_value.id]
       p << pricing_value.name
@@ -43,6 +46,32 @@ class PricingComponentsController < ApplicationController
     @pricing_component = PricingComponent.find(params[:id])
     make_a_table
     send_data @table.map{|x| x.shift; gencsv(x)}.join(""), :type => 'text/csv; charset=utf-8; header=present', :disposition => "attachment; filename=#{@pricing_component.name}.csv", :filename => "#{@pricing_component.name}.csv"
+  end
+
+  def import_table
+    @pricing_component = PricingComponent.find(params[:id])
+    prep_table_headers
+    @struct = OpenStruct.new(params[:open_struct])
+    results = CSV.parse(@struct.csv.read)
+    if results[0] == @table[0][1..-1]
+      @pricing_component.pricing_values.destroy_all
+      header = results.shift
+      results.each{|args|
+        h = {}
+        args.each_with_index {|x,i|
+          h[header[i]] = x
+        }
+        h['Maximum'] = h['Maximum'].to_i if header.include?('Maximum')
+        h['Minimum'] = h['Minimum'].to_i if header.include?('Minimum')
+        h['Value'] = h['Value'].to_s if header.include?('Value')
+        PricingValue.new(:pricing_component => @pricing_component, :name => h['Name'], :matcher => h['Matcher'], :minimum => h['Minimum'], :maximum => h['Maximum'], :value => h['Value']).save
+        # just process it
+      }
+      redirect_to :action => "edit", :id => @pricing_component.id
+    else
+      flash[:error] = "The column headers(#{results[0].join(", ")}) did not match expected: #{@table[0][1..-1].join(", ")}"
+      redirect_to :action => "edit", :id => @pricing_component.id
+    end
   end
 
   def new
