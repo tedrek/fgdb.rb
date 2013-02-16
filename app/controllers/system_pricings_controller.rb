754f362de503f0715f321faa9a9ac5aa60108eeb
Ryan Niebur <ryanryan52@gmail.com>
2013-02-16 11:01:45 -0800

    remember which letter I used for a variable name.. I should have made a local test case

--- app/controllers/system_pricings_controller.rb
+++ app/controllers/system_pricings_controller.rb
@@ -36,7 +36,7 @@ class SystemPricingsController < ApplicationController
     end
     render :update do |page|
       @system_pricing.pricing_values.select{|x| x.pricing_component.lookup_type.to_s.length > 0}.each do |c|
-        page << '$("pricing_values_for_' + x.pricing_component_id + '").value = ' + c.name.to_json + ';'
+        page << '$("pricing_values_for_' + c.pricing_component_id + '").value = ' + c.name.to_json + ';'
       end
       page.hide loading_indicator_id("calculated_price")
       page << '$("calculated_price").innerHTML = "$' + @system_pricing.calculated_price + '";'
