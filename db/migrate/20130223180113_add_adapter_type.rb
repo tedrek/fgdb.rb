fb73e64621fa5973a259a6cd190bb8ec3ac918e1
Ryan Niebur <ryanryan52@gmail.com>
2013-02-23 10:15:15 -0800

    Add a type for Adapters in sales

--- db/migrate/20130223180113_add_adapter_type.rb
+++ db/migrate/20130223180113_add_adapter_type.rb
@@ -1,5 +1,17 @@
 class AddAdapterType < ActiveRecord::Migration
   def self.up
+    ad = GizmoType.new
+    ad.gizmo_category = GizmoCategory.find_by_name('misc')
+    ad.parent_name = 'gizmo'
+    ad.suggested_fee_cents = 0
+    ad.required_fee_cents = 0
+    ad.covered = false
+    ad.needs_id = false
+    ad.rank = 99
+    ad.name = 'adapter'
+    ad.description = 'Adapter'
+    ad.gizmo_contexts = [GizmoContext.sale]
+    ad.save!
   end
 
   def self.down
