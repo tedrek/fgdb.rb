d9718a4aaa429e3d295b9b6c7f8ace7c58b03e37
Ryan Niebur <ryanryan52@gmail.com>
2013-03-02 09:00:21 -0800

    Set box_source name to Hardware Grant when instead of HW Grants for consistency

--- db/migrate/20130302164359_make_data_match.rb
+++ db/migrate/20130302164359_make_data_match.rb
@@ -1,5 +1,6 @@
 class MakeDataMatch < ActiveRecord::Migration
   def self.up
+    DB.exec("UPDATE warranty_lengths SET box_source = 'Hardware Grant' WHERE box_source LIKE 'HW Grants';")
   end
 
   def self.down
