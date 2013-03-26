e53c4bda7b5ecfa4ca208ab2769ac144b7333f7c
Ryan Niebur <ryanryan52@gmail.com>
2013-03-26 11:10:54 -0700

    fix privilege test

--- app/controllers/spec_sheets_controller.rb
+++ app/controllers/spec_sheets_controller.rb
@@ -27,7 +27,7 @@ class SpecSheetsController < ApplicationController
       s = SpecSheet.find(params[:id])
       # if no admins, only people with actual build_instructor role, do this: u.privileges.include?(required_privileges("show/sign_off").flatten.first)
           # do not allow when users is the contact for the BT
-      if u.contact_id != s.contact_id && u.has_privileges(required_privileges("sign_off").flatten.first)
+      if u.contact_id != s.contact_id && u.has_privileges(required_privileges("show/sign_off").flatten.first)
         s.signed_off_by=(u)
         s.save!
       end
