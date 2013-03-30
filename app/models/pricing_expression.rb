f5a87d1e1846474bfe572485ae3da6d068efc86d
root <root@zhora.fglan>
2013-03-30 14:17:00 -0700

    fix more bugs

--- app/models/pricing_expression.rb
+++ app/models/pricing_expression.rb
@@ -5,24 +5,28 @@ class PricingExpression < ActiveRecord::Base
 
   def amount(pricing_values)
     v = [self.multiplier_cents]
+    seen = []
     pricing_values.each do |value|
       if self.pricing_components.include?(value.pricing_component)
         v << (value.pricing_component.multiplier_cents * value.value_cents/10000.0)
+        seen << value.pricing_component unless seen.include?(value.pricing_component)
       end
     end
-    return 0.0 if v.length == 1
+    return 0.0 if self.pricing_components.length != seen.length
     return v.inject(1.0){|t,x| t = t * x }
   end
 
   def to_equation_text
     a = self.multiplier_cents == 100 ? [] : [self.multiplier]
     a = a + self.pricing_components.map{|x| x.to_equation_text}
+    a = [self.multiplier] if a.length == 0
     return a.join(' * ')
   end
 
   def to_equation(pricing_values)
     a = self.multiplier_cents == 100 ? [] : [self.multiplier]
     a = a + self.pricing_components.map{|x| x.to_equation(pricing_values)}
+    a = [self.multiplier] if a.length == 0
     return a.join(' * ')
   end
 end
