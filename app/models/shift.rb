323be82f53a8ef14d2c0a75dc3a21dbfa0f238d2
Ryan Niebur <ryanryan52@gmail.com>
2013-04-14 08:32:54 -0700

    Fix bug that caused duplicate work shift footnotes, which made some disappear randomly (Closes: #34982)

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -97,9 +97,8 @@ class Shift < ActiveRecord::Base
         root_sched = Schedule.generate_from
         in_clause = root_sched.in_clause
         if footnote = ShiftFootnote.find_by_schedule_id_and_weekday_id(root_sched.id, weekday_id)
-          wsf = WorkShiftFootnote.new
+          wsf = WorkShiftFootnote.find_by_date(day) || WorkShiftFootnote.new(:date => day)
           wsf.note = footnote.note
-          wsf.date = day
           wsf.save!
         else
           wsf = WorkShiftFootnote.find_by_date(day)
