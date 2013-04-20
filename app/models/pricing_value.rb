d2cea9eb943d307a48d53d4325f36bbe0fd6acd5
Ryan Niebur <ryanryan52@gmail.com>
2013-04-20 11:20:36 -0700

    fix matching to check the most specific results first

--- app/models/pricing_value.rb
+++ app/models/pricing_value.rb
@@ -19,12 +19,15 @@ class PricingValue < ActiveRecord::Base
     self.replaced_by ? self.replaced_by.finally_replaced_by : self
   end
 
+  def match_against
+    (self.matcher && self.matcher.length > 0) ? self.matcher : self.name
+  end
+
   def matches?(value)
     if self.pricing_component && self.pricing_component.numerical && self.minimum && self.maximum
       i = value.to_i
       return i >= self.minimum && i <= self.maximum
     else
-      match_against = (self.matcher && self.matcher.length > 0) ? self.matcher : self.name
       return SystemPricing.does_match?(match_against, value)
     end
   end
