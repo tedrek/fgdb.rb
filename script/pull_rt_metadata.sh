539f15d3961251f12b5d28c27c7ac24d1e58ab14
Ryan Niebur <ryanryan52@gmail.com>
2013-05-31 17:09:12 -0700

    do not blow away rt_metadata.txt if generating fails, even silently (empty result file)

--- script/pull_rt_metadata.sh
+++ script/pull_rt_metadata.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-sudo ssh -i /root/.ssh/wasp_key root@art /root/fgdb.rb/script/generate_rt_metadata.sh > /var/www/fgdb.rb/config/rt_metadata.txt
+set -e
+
+TEMPF=$(mktemp)
+
+sudo ssh -i /root/.ssh/wasp_key root@art /root/fgdb.rb/script/generate_rt_metadata.sh > $TEMPF
+sudo chown www-data:www-data "$TEMPF"
+if [ -n "$(cat $TEMPF)" ]; then
+    mv $TEMPF /var/www/fgdb.rb/config/rt_metadata.txt
+fi
 
