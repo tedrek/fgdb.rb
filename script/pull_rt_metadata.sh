bab7658d6c01705fe6235fd22f220ab3d369a020
Ryan Niebur <ryanryan52@gmail.com>
2013-06-08 12:31:06 -0700

    644, not 640

--- script/pull_rt_metadata.sh
+++ script/pull_rt_metadata.sh
@@ -6,7 +6,7 @@ TEMPF=$(mktemp)
 
 sudo ssh -i /root/.ssh/wasp_key root@art /root/fgdb.rb/script/generate_rt_metadata.sh > $TEMPF
 sudo chown www-data:www-data "$TEMPF"
-sudo chmod 640 "$TEMPF"
+sudo chmod 644 "$TEMPF"
 if [ -n "$(cat $TEMPF)" ]; then
     mv $TEMPF /var/www/fgdb.rb/config/rt_metadata.txt
 fi
