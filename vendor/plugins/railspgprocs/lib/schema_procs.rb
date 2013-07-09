4a41b89ed991792856aa91067a1cfd1704473ff5
Tyler Hamilton <tyler@freegeekvancouver.org>
2013-07-09 11:00:11 -0700

    Patch railspgprocs for ruby1.9

--- vendor/plugins/railspgprocs/lib/schema_procs.rb
+++ vendor/plugins/railspgprocs/lib/schema_procs.rb
@@ -2,7 +2,9 @@ module SchemaProcs
   @@_cascade_or_restrict = Proc.new {|which| which     ? 'CASCADE' : 'RESTRICT' }
   @@_strict_or_null      = Proc.new {|strict| strict   ? 'STRICT' : 'CALLED ON NULL INPUT' }
   @@_definer_or_invoker  = Proc.new {|definer| definer ? 'DEFINER' : 'INVOKER' }
-  @@_behavior            = Proc.new {|volatile| %w{immutable stable volatile}.grep(/^#{volatile[0,1]}.+/).to_s }
+  @@_behavior            = Proc.new do |volatile|
+    %w{immutable stable volatile}.grep(/^#{volatile[0,1]}.+/).join(' ')
+  end
 
   def cascade_or_restrict(cascade=false)
     @@_cascade_or_restrict.call(cascade)
