7aafea64f8d8e670ef2b6303d39473ffb41a98b0
Tyler Hamilton <tyler@freegeekvancouver.org>
2013-09-04 17:50:37 -0700

    Replace deprecated Errors#on calls

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -17,28 +17,28 @@ class UserTest < ActiveSupport::TestCase
   def test_should_require_login
     assert_no_difference 'User.count' do
       u = create_user(:login => nil)
-      assert u.errors.on(:login)
+      assert u.errors[:login].any?
     end
   end
 
   def test_should_require_password
     assert_no_difference 'User.count' do
       u = create_user(:password => nil)
-      assert u.errors.on(:password)
+      assert u.errors[:password].any?
     end
   end
 
   def test_should_require_password_confirmation
     assert_no_difference 'User.count' do
       u = create_user(:password_confirmation => nil)
-      assert u.errors.on(:password_confirmation)
+      assert u.errors[:password_confirmation].any?
     end
   end
 
   def test_should_require_email
     assert_no_difference 'User.count' do
       u = create_user(:email => nil)
-      assert u.errors.on(:email)
+      assert u.errors[:email].any?
     end
   end
 
