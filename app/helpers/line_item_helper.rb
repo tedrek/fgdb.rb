c64441f873487a8066c865a713df4e25ef821128
Tyler Hamilton <tyler@freegeekvancouver.org>
2013-12-13 11:08:14 -0800

    Wrap line_item() helper and explicit html_safe

--- app/helpers/line_item_helper.rb
+++ app/helpers/line_item_helper.rb
@@ -13,7 +13,11 @@ module LineItemHelper
   end
 
   def line_item(prefix, klass, form_oh, values = [], add_link = false)
-    line_item_form_for(prefix, form_oh, add_link).to_s + line_item_instance_js(prefix, klass) + ((values.length > 0) ? line_item_add_many(prefix, values) : "")
+    ret = "".html_safe
+    ret << line_item_form_for(prefix, form_oh, add_link)
+    ret << line_item_instance_js(prefix, klass)
+    ret << ((values.length > 0) ? line_item_add_many(prefix, values) : "")
+    return ret
   end
 
   include TableHelper
