8f32c89949726a39b9af84e6431d607ba3986b4b
Tyler Hamilton <tyler@freegeekvancouver.org>
2013-12-15 12:23:54 -0800

    Fix donations helpers

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -53,4 +53,24 @@ class DonationsController < TransactionController
   def default_condition
     "created_at"
   end
+
+  helper_method :base_controller, :columns
+  def base_controller
+    return '/donations'
+  end
+
+  def columns
+    [
+     Column.new(Donation, :name => 'id'),
+     Column.new(Donation, :name => 'payment',
+                :eval  => 'donation.payment', :sortable => false),
+     Column.new(Donation, :name => 'donor', :sortable => false,
+                :eval => 'donation.donor'),
+     Column.new(Donation, :name => 'gizmos', :sortable => false),
+     Column.new(Donation, :name => 'reported_required_fee', :label => 'Required Fee'),
+     Column.new(Donation, :name => 'reported_suggested_fee', :label => 'Suggested Fee'),
+     Column.new(Donation, :name => 'occurred_at'),
+    ]
+  end
+
 end
--- app/helpers/donations_helper.rb
+++ app/helpers/donations_helper.rb
@@ -1,24 +1,3 @@
 module DonationsHelper
   include TransactionHelper
-
-  def base_controller
-    return '/donations'
-  end
-
-  # Ensure the column list is named uniquely
-  # all helpers are included in all views by default
-  # http://stackoverflow.com/q/1179865
-  def donation_columns
-    [
-     Column.new(Donation, :name => 'id'),
-     Column.new(Donation, :name => 'payment',
-                :eval  => 'donation.payment', :sortable => false),
-     Column.new(Donation, :name => 'donor', :sortable => false,
-                :eval => 'donation.donor'),
-     Column.new(Donation, :name => 'gizmos', :sortable => false),
-     Column.new(Donation, :name => 'reported_required_fee', :label => 'Required Fee'),
-     Column.new(Donation, :name => 'reported_suggested_fee', :label => 'Suggested Fee'),
-     Column.new(Donation, :name => 'occurred_at'),
-    ]
-  end
 end
