e7877f76ae89593f9351be913b8bda2fc50427eb
Tyler Hamilton <tyler@freegeekvancouver.org>
2014-02-24 16:46:04 -0800

    Simplify VolunteerTaskType selects

--- app/models/volunteer_task_type.rb
+++ app/models/volunteer_task_type.rb
@@ -3,20 +3,21 @@ class VolunteerTaskType < ActiveRecord::Base
 
   has_many :contact_volunteer_task_type_counts
 
-  def self.evaluation_type
-    @@eval_type ||= VolunteerTaskType.find_by_name('evaluation')
-  end
-
-  def to_s
-    description
-  end
-
+  # What is/was the purpose of this?
   scope :instantiables, where(:instantiable => true)
   scope :effective_on, lambda { |date|
     where('(effective_on IS NULL OR effective_on <= :date) AND
            (ineffective_on IS NULL OR ineffective_on > :date)',
           {:date => date})
   }
+
+  scope :current, instantiables.
+    effective_on(Time.zone.today).
+    order(:description)
+
+  def self.evaluation_type
+    @@eval_type ||= VolunteerTaskType.find_by_name('evaluation')
+  end
 
   def self.find_actual(*ids)
     ids.delete_if {|id| id == 0 }
