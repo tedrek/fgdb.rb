8fa1d5126f19962aadc7800f7f3153f7487a12bc
Tyler Hamilton <tyler@freegeekvancouver.org>
2014-03-01 11:41:30 -0800

    Allow JSON error messages, consistent use for partial

--- app/controllers/api/v0/drives_controller.rb
+++ app/controllers/api/v0/drives_controller.rb
@@ -5,6 +5,7 @@ module Api
         @drive = Drive.create(params[:drive])
         if @drive.save
           render '_drive', {
+            locals: {drive: @drive},
             format: :json,
             status: :created,
             location: "/api/v0/drives/#{@drive.id}",
@@ -22,14 +23,19 @@ module Api
       end
 
       def show
-        @drive = Drive.find(params[:id])
-        render '_drive', format: :json
+        begin
+          @drive = Drive.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+          render '_error', format: :json, locals: {errors: ['Record not found']}
+          return
+        end
+        render '_drive', format: :json, locals: {drive: @drive}
       end
 
       def update
         @drive = Drive.find(params[:id])
         @drive.update_attributes!(params[:drive])
-        render '_drive', format: :json
+        render '_drive', format: :json, locals: {drive: @drive}
       end
     end
   end
