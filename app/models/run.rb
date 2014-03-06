b10dbbbe2729c7fced09af444d4e53baa772e6bd
Tyler Hamilton <tyler@freegeekvancouver.org>
2014-03-06 15:47:26 -0800

    Add validation to Run

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,4 +1,8 @@
 class Run < ActiveRecord::Base
   belongs_to :drive
   has_many   :checks
+
+  validates :drive, presence: true
+  validates :device_name, presence: true
+  validates :start_time, presence: true
 end
